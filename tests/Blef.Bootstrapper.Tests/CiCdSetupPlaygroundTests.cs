f20598f20a7492ab6a0f6f79e7fe951e5236f4df
TeoVincent <artur.wincenciak@gmail.com>
2022-08-11 21:27:16 +0200

    Continues integration playground tests

---
+++ tests/Blef.Bootstrapper.Tests/CiCdSetupPlaygroundTests.cs
@@ -0,0 +1,12 @@
+namespace Blef.Bootstrapper.Tests;
+
+public class ContinuesIntegrationPlaygroundTests
+{
+    [Fact]
+    public void AlwaysGreen() =>
+        Assert.True(true);
+
+    [Fact]
+    public void AlwaysRed() =>
+        Assert.True(false);
+}
