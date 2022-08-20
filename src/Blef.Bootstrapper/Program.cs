e578a1a51aebadaf03efb4cc1383d89d8adff590
TeoVincent <artur.wincenciak@gmail.com>
2022-08-20 23:37:20 +0200

    Encapsulating shared infra with added and mapped internal controllers

--- src/Blef.Bootstrapper/Program.cs
+++ src/Blef.Bootstrapper/Program.cs
@@ -1,19 +1,11 @@
+using Blef.Shared.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseInfrastructure();
 
 app.Run();
