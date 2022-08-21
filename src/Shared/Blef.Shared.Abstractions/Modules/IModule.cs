050ca392a7b66557abd48b5c0f387bc44be1b8c1
TeoVincent <artur.wincenciak@gmail.com>
2022-08-21 13:59:13 +0200

    Module loader contract without implementation

---
+++ src/Shared/Blef.Shared.Abstractions/Modules/IModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blef.Shared.Abstractions.Modules;
+
+public interface IModule
+{
+    string Name { get; }
+    string Path { get; }
+    IEnumerable<string> Policies { get; }
+    void Register(IServiceCollection services);
+    void Use(IApplicationBuilder app);
+}
