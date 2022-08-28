4a5c309b20402b48b34107007cd40d3fb3ec415a
TeoVincent <artur.wincenciak@gmail.com>
2022-08-28 15:10:20 +0200

    Games base controller with default route

---
+++ src/Modules/Games/Blef.Modules.Games.Api/Controllers/GamesControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blef.Modules.Games.Api.Controllers;
+
+[ApiController]
+[Route($"{GamesModule.BasePath}/[controller]")]
+internal abstract class GamesControllerBase : ControllerBase
+{
+
+}
