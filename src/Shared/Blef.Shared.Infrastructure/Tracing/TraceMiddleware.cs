d86e2c82410c776571934df8a61e2efe3d105180
TeoVincent <artur.wincenciak@gmail.com>
2022-09-04 10:30:06 +0200

    Naming convention of header tracing property names

--- src/Shared/Blef.Shared.Infrastructure/Tracing/TraceMiddleware.cs
+++ src/Shared/Blef.Shared.Infrastructure/Tracing/TraceMiddleware.cs
@@ -7,8 +7,8 @@ internal class TraceMiddleware : IMiddleware
 {
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        context.Response.Headers.Add("traceId", context.TraceIdentifier);
-        context.Response.Headers.Add("activityId", Activity.Current?.Id!);
+        context.Response.Headers.Add("Trace-Id", context.TraceIdentifier);
+        context.Response.Headers.Add("Activity-Id", Activity.Current?.Id!);
         await next(context);
     }
 }
