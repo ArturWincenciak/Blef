ec85471033903ce1d18cab6e1dd04b2df2ffc499
TeoVincent <artur.wincenciak@gmail.com>
2022-09-16 19:05:10 +0200

    Query shared abstraction

---
+++ src/Shared/Blef.Shared.Abstractions/Queries/IQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace Blef.Shared.Abstractions.Queries;
+
+public interface IQuery
+{
+}
+
+public interface IQuery<TResult> : IQuery
+    where TResult : IQueryResult
+{
+}
---
+++ src/Shared/Blef.Shared.Abstractions/Queries/IQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Blef.Shared.Abstractions.Queries;
+
+public interface IQueryHandler<in TQuery, TResult>
+    where TQuery : IQuery<TResult>
+    where TResult : IQueryResult
+{
+    Task<TResult> HandleAsync(TQuery query);
+}
