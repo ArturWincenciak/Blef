18dda2bb027ce07b9b58e537fc386a8bb41d5250
TeoVincent <artur.wincenciak@gmail.com>
2022-09-17 12:42:27 +0200

    Just command handling playground

---
+++ src/Modules/Games/Blef.Modules.Games.Application/ErrorHandlingPlayground/Commands/JustCommand.cs
@@ -0,0 +1,8 @@
+﻿using Blef.Shared.Abstractions.Commands;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blef.Modules.Games.Application.ErrorHandlingPlayground.Commands;
+
+public sealed record JustCommand(
+    [Required] [MinLength(3)] [MaxLength(5)] string Stuff,
+    [Required] [Range(3, 6)] int Count) : ICommand;
