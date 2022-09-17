043647bb9a47c8e0996a0d5f81585ce820e355df
TeoVincent <artur.wincenciak@gmail.com>
2022-09-17 13:26:35 +0200

    All command is a record struct

--- src/Modules/Games/Blef.Modules.Games.Application/ErrorHandlingPlayground/Commands/JustCommand.cs
+++ src/Modules/Games/Blef.Modules.Games.Application/ErrorHandlingPlayground/Commands/JustCommand.cs
@@ -3,6 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Blef.Modules.Games.Application.ErrorHandlingPlayground.Commands;
 
-public sealed record JustCommand(
+public record struct JustCommand(
     [Required] [MinLength(3)] [MaxLength(5)] string Stuff,
     [Required] [Range(3, 6)] int Count) : ICommand;
