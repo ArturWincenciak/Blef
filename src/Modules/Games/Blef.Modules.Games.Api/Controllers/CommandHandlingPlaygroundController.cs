70f9dc81a6291f72358c93c27d6a5983a8aa40b2
TeoVincent <artur.wincenciak@gmail.com>
2022-09-17 12:42:27 +0200

    Just command handling playground

---
+++ src/Modules/Games/Blef.Modules.Games.Api/Controllers/CommandHandlingPlaygroundController.cs
@@ -0,0 +1,17 @@
+﻿using Blef.Modules.Games.Application.ErrorHandlingPlayground.Commands;
+using Blef.Shared.Abstractions.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blef.Modules.Games.Api.Controllers;
+
+internal sealed class CommandHandlingPlaygroundController : GamesControllerBase
+{
+    private readonly ICommandDispatcher _commandDispatcher;
+
+    public CommandHandlingPlaygroundController(ICommandDispatcher commandDispatcher) =>
+        _commandDispatcher = commandDispatcher;
+
+    [HttpPost]
+    public async Task<IActionResult> Post(JustCommand command) =>
+        await Ok(() => _commandDispatcher.SendAsync(command));
+}
---
+++ src/Modules/Games/Blef.Modules.Games.Application/ErrorHandlingPlayground/Commands/Handlers/JustCommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using Blef.Shared.Abstractions.Commands;
+
+namespace Blef.Modules.Games.Application.ErrorHandlingPlayground.Commands.Handlers;
+
+internal sealed class JustCommandHandler : ICommandHandler<JustCommand>
+{
+    public Task HandleAsync(JustCommand command) =>
+        Task.CompletedTask;
+}
