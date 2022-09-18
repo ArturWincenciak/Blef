d56d6e0258b3edb8e8db90150768d07e33674856
TeoVincent <artur.wincenciak@gmail.com>
2022-09-18 09:48:52 +0200

    Playground effect command

---
+++ src/Modules/Games/Blef.Modules.Games.Application/Playground/Commands/EffectCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Blef.Shared.Abstractions.Commands;
+
+namespace Blef.Modules.Games.Application.Playground.Commands;
+
+public sealed record EffectCommand(
+    [Required] [Range(1, int.MaxValue)] int Amount,
+    [Required] bool Flag) : ICommand<EffectCommand.Result>
+{
+    public sealed record Result(string[] Effect) : ICommandResult;
+}
---
+++ src/Modules/Games/Blef.Modules.Games.Application/Playground/Commands/Handlers/EffectCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Blef.Shared.Abstractions.Commands;
+
+namespace Blef.Modules.Games.Application.Playground.Commands.Handlers;
+
+internal sealed class EffectCommandHandler : ICommandHandler<EffectCommand, EffectCommand.Result>
+{
+    public async Task<EffectCommand.Result> Handle(EffectCommand command, CancellationToken cancellation)
+    {
+        var collection = new string [command.Amount];
+        Array.Fill(collection, command.Flag ? "Donald Knuth" : "Surreal Numbers");
+        return await Task.FromResult(new EffectCommand.Result(collection));
+    }
+}
