a7e15b4d47f33dced573e4ebfae159a95b6052c1
TeoVincent <artur.wincenciak@gmail.com>
2022-10-02 18:30:30 +0200

    Just query CQRS playground example

---
+++ src/Modules/Games/Blef.Modules.Games.Application/Playground/Queries/JustQuery.cs
@@ -0,0 +1,11 @@
+﻿using Blef.Shared.Abstractions.Queries;
+
+namespace Blef.Modules.Games.Application.Playground.Queries;
+
+public sealed record JustQuery(
+    int Id) : IQuery<JustQuery.Result>
+{
+    public sealed record Result(
+        string JustResult,
+        string JustDescription) : IQueryResult;
+}
