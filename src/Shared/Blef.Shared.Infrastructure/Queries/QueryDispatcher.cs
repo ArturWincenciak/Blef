08783e0e112459fefbee3978e7f9a55d110fc2de
TeoVincent <artur.wincenciak@gmail.com>
2022-10-02 18:29:42 +0200

    Add logs in query dispatcher

--- src/Shared/Blef.Shared.Infrastructure/Queries/QueryDispatcher.cs
+++ src/Shared/Blef.Shared.Infrastructure/Queries/QueryDispatcher.cs
@@ -1,22 +1,33 @@
 ﻿using Blef.Shared.Abstractions.Queries;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Blef.Shared.Infrastructure.Queries;
 
 internal class QueryDispatcher : IQueryDispatcher
 {
+    private readonly ILogger<QueryDispatcher> _logger;
     private readonly IServiceProvider _serviceProvider;
 
-    public QueryDispatcher(IServiceProvider serviceProvider)
+    public QueryDispatcher(IServiceProvider serviceProvider, ILogger<QueryDispatcher> logger)
     {
         _serviceProvider = serviceProvider;
+        _logger = logger;
     }
 
     public async Task<TQueryResult> Dispatch<TQuery, TQueryResult>(TQuery query, CancellationToken cancellation)
         where TQuery : IQuery<TQueryResult>
         where TQueryResult : IQueryResult
     {
-        var handler = _serviceProvider.GetRequiredService<IQueryHandler<TQuery, TQueryResult>>();
-        return await handler.Handle(query);
+        try
+        {
+            var handler = _serviceProvider.GetRequiredService<IQueryHandler<TQuery, TQueryResult>>();
+            return await handler.Handle(query);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "{@query}", query);
+            throw;
+        }
     }
 }
