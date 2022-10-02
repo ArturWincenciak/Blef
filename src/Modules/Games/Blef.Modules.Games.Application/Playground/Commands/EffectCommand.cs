1f539f562bb285a57ca4fa69eb0fd7409fea73f8
TeoVincent <artur.wincenciak@gmail.com>
2022-10-02 18:30:44 +0200

    Effect command validation

--- src/Modules/Games/Blef.Modules.Games.Application/Playground/Commands/EffectCommand.cs
+++ src/Modules/Games/Blef.Modules.Games.Application/Playground/Commands/EffectCommand.cs
@@ -4,7 +4,7 @@ using Blef.Shared.Abstractions.Commands;
 namespace Blef.Modules.Games.Application.Playground.Commands;
 
 public sealed record EffectCommand(
-    [Required] [Range(1, int.MaxValue)] int Amount,
+    [Required] [Range(1, 32)] int Amount,
     [Required] bool Flag) : ICommand<EffectCommand.Result>
 {
     public sealed record Result(string[] Effect) : ICommandResult;
