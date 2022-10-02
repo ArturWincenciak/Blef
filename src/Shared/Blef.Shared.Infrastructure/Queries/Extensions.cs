34f412fea6bb6415083bc64ba6dbb4f752818a32
TeoVincent <artur.wincenciak@gmail.com>
2022-10-02 17:24:06 +0200

    Query dispatcher implementation

---
+++ src/Shared/Blef.Shared.Infrastructure/Queries/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Blef.Shared.Abstractions.Queries;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blef.Shared.Infrastructure.Queries;
+
+internal static class Extensions
+{
+    public static IServiceCollection AddQueries(this IServiceCollection services, IEnumerable<Assembly> assemblies) =>
+        services
+            .AddScoped<IQueryDispatcher, QueryDispatcher>()
+            .Scan(typeSourceSelector => typeSourceSelector.FromAssemblies(assemblies)
+                .AddClasses(filter => filter.AssignableTo(typeof(IQueryHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+}
