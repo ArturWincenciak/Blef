82ba450295d5d989590fd1faab73b4fb331dcd74
TeoVincent <artur.wincenciak@gmail.com>
2022-10-07 13:46:38 +0200

    Move games to domain project

--- src/Modules/Games/Blef.Modules.Games.Application/Commands/Handlers/MakeNewGameHandler.cs
+++ src/Modules/Games/Blef.Modules.Games.Application/Commands/Handlers/MakeNewGameHandler.cs
@@ -4,9 +4,15 @@ namespace Blef.Modules.Games.Application.Commands.Handlers;
 
 internal sealed class MakeNewGameHandler : ICommandHandler<MakeNewGame, MakeNewGame.Result>
 {
+    private readonly Domain.Games _games;
+
+    public MakeNewGameHandler(Domain.Games games) =>
+        _games = games;
+
     public async Task<MakeNewGame.Result> Handle(MakeNewGame command, CancellationToken cancellation)
     {
-        var result = new MakeNewGame.Result(Guid.NewGuid());
+        var gameId = _games.MakeNewGame();
+        var result = new MakeNewGame.Result(gameId);
         return await Task.FromResult(result);
     }
 }
