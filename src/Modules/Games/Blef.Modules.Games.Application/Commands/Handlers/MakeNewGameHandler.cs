77ed0522ebc8f0d04939c2e42f5ae60c4b677143
TeoVincent <artur.wincenciak@gmail.com>
2022-10-07 13:37:30 +0200

    Refactor to make new game command handler

---
+++ src/Modules/Games/Blef.Modules.Games.Application/Commands/Handlers/MakeNewGameHandler.cs
@@ -0,0 +1,12 @@
+﻿using Blef.Shared.Abstractions.Commands;
+
+namespace Blef.Modules.Games.Application.Commands.Handlers;
+
+internal sealed class MakeNewGameHandler : ICommandHandler<MakeNewGame, MakeNewGame.Result>
+{
+    public async Task<MakeNewGame.Result> Handle(MakeNewGame command, CancellationToken cancellation)
+    {
+        var result = new MakeNewGame.Result(Guid.NewGuid());
+        return await Task.FromResult(result);
+    }
+}
---
+++ src/Modules/Games/Blef.Modules.Games.Application/Commands/MakeNewGame.cs
@@ -0,0 +1,8 @@
+﻿using Blef.Shared.Abstractions.Commands;
+
+namespace Blef.Modules.Games.Application.Commands;
+
+public sealed record MakeNewGame : ICommand<MakeNewGame.Result>
+{
+    public sealed record Result(Guid GameId) : ICommandResult;
+}
