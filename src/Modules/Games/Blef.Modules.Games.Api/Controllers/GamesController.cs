0a56884dba802968fba8e8950158cd1f6d19b3e7
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2022-10-07 07:38:15 +0200

    feat: #56 Create new game

---
+++ src/Modules/Games/Blef.Modules.Games.Api/Controllers/GamesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blef.Modules.Games.Api.Controllers;
+
+[Route(GamesModule.BasePath)]
+internal sealed class GamesController : GamesControllerBase
+{
+    private static readonly Blef.Modules.Games.Application.Games _games = new();
+
+    [HttpPost]
+    public ActionResult<string> CreateGame() =>
+        JsonSerializer.Serialize(new
+        {
+            gameId = _games.CreateGame()
+        }, new JsonSerializerOptions { WriteIndented = true });
+}
