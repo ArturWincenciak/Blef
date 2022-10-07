5d4c103e29017e53e904efc22a56bf9bda532e6f
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2022-10-07 20:43:15 +0200

    feat: #56 contract for get cards

---
+++ src/Modules/Games/Blef.Modules.Games.Application/Queries/Handlers/GetPlayerCards.cs
@@ -0,0 +1,12 @@
+﻿using Blef.Modules.Games.Application.Queries;
+using Blef.Modules.Games.Domain;
+using Blef.Shared.Abstractions.Queries;
+
+internal sealed class GetPlayerCardsHandler : IQueryHandler<GetPlayerCards, GetPlayerCards.Result>
+{
+    public Task<GetPlayerCards.Result> Handle(GetPlayerCards query)
+    {
+        var card = new Card("Ace", "Diamonds");
+        return Task.FromResult(new GetPlayerCards.Result(new[] { card }));
+    }
+}
---
+++ src/Modules/Games/Blef.Modules.Games.Domain/Card.cs
@@ -0,0 +1,3 @@
+namespace Blef.Modules.Games.Domain;
+
+public record Card(string FaceCard, string Suit);
