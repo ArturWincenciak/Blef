82be51bec9fb496e0b7161b34d06f6a0c45b3039
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2022-10-08 16:22:10 +0200

    feat: #60 Check previous poker hand and Get Looser

---
+++ src/Modules/Games/Blef.Modules.Games.Application/Commands/Handlers/CheckHandler.cs
@@ -0,0 +1,18 @@
+﻿using Blef.Shared.Abstractions.Commands;
+
+namespace Blef.Modules.Games.Application.Commands.Handlers;
+
+internal sealed class CheckHandler : ICommandHandler<Check>
+{
+    private readonly Domain.Games _games;
+
+    public CheckHandler(Domain.Games games) =>
+        _games = games;
+
+    public Task Handle(Check command, CancellationToken cancellation)
+    {
+        var game = _games.GetExistingGame(command.GameId);
+        game.Check(command.PlayerId);
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Modules/Games/Blef.Modules.Games.Application/Queries/Handlers/GetLooserHandler.cs
@@ -0,0 +1,20 @@
+﻿using Blef.Modules.Games.Application.Queries;
+using Blef.Modules.Games.Domain;
+using Blef.Shared.Abstractions.Queries;
+
+internal sealed class GetLooserHandler : IQueryHandler<GetLooser, GetLooser.Result>
+{
+    private readonly Games _games;
+
+    public GetLooserHandler(Games games)
+    {
+        _games = games;
+    }
+
+    public Task<GetLooser.Result> Handle(GetLooser query)
+    {
+        var game = _games.GetExistingGame(query.GameId);
+        var looser = game.GetLooser();
+        return Task.FromResult(new GetLooser.Result(looser));
+    }
+}
