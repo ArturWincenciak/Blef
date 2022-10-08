1ff7bbff31307b74f5887d45e8f426879a40a7df
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2022-10-08 14:31:23 +0200

    feat: #58 Bid

--- src/Modules/Games/Blef.Modules.Games.Domain/Game.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Game.cs
@@ -2,16 +2,22 @@ namespace Blef.Modules.Games.Domain;
 
 internal sealed class Game
 {
-    private readonly Dictionary<Guid, Card[]> _players = new();
+    private readonly Dictionary<Guid, Player> _players = new();
 
     public void Join(Guid playerId)
     {
         var card = new Card("Ace", "Diamonds");
-        _players.Add(playerId, new[] { card });
+        _players.Add(playerId, new Player(new[] { card }));
     }
 
     public Card[] GetCards(Guid playerId)
     {
-        return _players[playerId];
+        return _players[playerId].DealtCards;
+    }
+
+    public void Bid(Guid playerId, string pokerHand)
+    {
+        // TOOD: check if bid is better than current one
+        _players[playerId].Bid(pokerHand);
     }
 }
