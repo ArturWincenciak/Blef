4dd00a497faa20767f4d3cb2269cfcd9ea090934
TeoVincent <artur.wincenciak@gmail.com>
2022-10-08 23:26:43 +0200

    Group controller and API commands in directory

--- src/Modules/Games/Blef.Modules.Games.Api/Controllers/ModuleControllerBase.cs
+++ src/Modules/Games/Blef.Modules.Games.Api/Controllers/ModuleControllerBase.cs
@@ -5,4 +5,4 @@ namespace Blef.Modules.Games.Api.Controllers;
 [ApiController]
 [Consumes("application/json")]
 [Route($"{GamesModule.BasePath}/[controller]")]
-internal abstract class GamesControllerBase : ControllerBase { }
+internal abstract class ModuleControllerBase : ControllerBase { }
