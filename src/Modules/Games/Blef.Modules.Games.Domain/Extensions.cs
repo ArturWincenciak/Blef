c19c71854a315f0751a57fb56a92862d9b1e935b
TeoVincent <artur.wincenciak@gmail.com>
2022-10-09 01:17:00 +0200

    Move games repository into infrastructure project

--- src/Modules/Games/Blef.Modules.Games.Domain/Extensions.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Extensions.cs
@@ -1,14 +1,14 @@
 ﻿using System.Runtime.CompilerServices;
-using Blef.Modules.Games.Domain.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: InternalsVisibleTo(assemblyName: "Blef.Modules.Games.Application")]
+[assembly: InternalsVisibleTo(assemblyName: "Blef.Modules.Games.Infrastructure")]
 
 namespace Blef.Modules.Games.Domain;
 
 internal static class Extensions
 {
     internal static IServiceCollection AddDomain(this IServiceCollection services) =>
-        services.AddSingleton<IGamesRepository, Games>();
+        services;
 }
 
