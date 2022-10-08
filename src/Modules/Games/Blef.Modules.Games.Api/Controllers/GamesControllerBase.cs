46f524029b80030652298449e1277afcbcd05a92
TeoVincent <artur.wincenciak@gmail.com>
2022-10-08 23:10:13 +0200

    Cleanup app template playgrounds

--- src/Modules/Games/Blef.Modules.Games.Api/Controllers/GamesControllerBase.cs
+++ src/Modules/Games/Blef.Modules.Games.Api/Controllers/GamesControllerBase.cs
@@ -5,11 +5,4 @@ namespace Blef.Modules.Games.Api.Controllers;
 [ApiController]
 [Consumes("application/json")]
 [Route($"{GamesModule.BasePath}/[controller]")]
-internal abstract class GamesControllerBase : ControllerBase
-{
-    protected async Task<IActionResult> Ok(Func<Task> funk)
-    {
-        await funk();
-        return Ok();
-    }
-}
+internal abstract class GamesControllerBase : ControllerBase { }
--- src/Modules/Games/Blef.Modules.Games.Application/Playground/Commands/Handlers/JustCommandHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using Blef.Shared.Abstractions.Commands;
-
-namespace Blef.Modules.Games.Application.Playground.Commands.Handlers;
-
-internal sealed class JustCommandHandler : ICommandHandler<JustCommand>
-{
-    public Task Handle(JustCommand command, CancellationToken cancellation) =>
-        Task.CompletedTask;
-}
