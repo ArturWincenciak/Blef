269f85fb575cf98a42302d21edb7a761238c74e9
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2022-10-15 12:20:54 +0200

    feat: #81 Start Tournament

---
+++ src/Modules/Games/Blef.Modules.Games.Application/Commands/Handlers/StartTournamentHandler.cs
@@ -0,0 +1,19 @@
+﻿using Blef.Modules.Games.Domain.Repositories;
+using Blef.Shared.Abstractions.Commands;
+
+namespace Blef.Modules.Games.Application.Commands.Handlers;
+
+internal sealed class StartTournamentHandler : ICommandHandler<StartTournament>
+{
+    private readonly ITournamentsRepository _tournaments;
+
+    public StartTournamentHandler (ITournamentsRepository tournaments) =>
+        _tournaments = tournaments;
+
+    public Task Handle(StartTournament command, CancellationToken cancellation)
+    {
+        var tournament = _tournaments.Get(command.TournamentId);
+        tournament.Start();
+        return Task.CompletedTask;
+    }
+}
