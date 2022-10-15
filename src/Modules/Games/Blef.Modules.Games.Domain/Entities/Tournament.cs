314669d6e7a2d09a6c356f20cf42523bdeff4b95
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2022-10-15 12:13:18 +0200

    feat: #81 implement join Tournament

--- src/Modules/Games/Blef.Modules.Games.Domain/Entities/Tournament.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Entities/Tournament.cs
@@ -1,7 +1,12 @@
+using Blef.Shared.Kernel.Exceptions;
+
 namespace Blef.Modules.Games.Domain.Entities;
 
 public class Tournament
 {
+    private readonly List<Guid> _players = new();
+    private bool _isTournamentStarted;
+
     private Tournament(Guid id)
     {
         Id = id;
@@ -14,6 +19,22 @@ public class Tournament
 
     public void Join(Guid playerId)
     {
-        throw new NotImplementedException();
+        if (_isTournamentStarted)
+        {
+            throw new SimpleBlefException("Cannot join tournament that is already started");
+        }
+        
+        // TODO: check, cause probably we can handle any number. Think about upper bound like 9?
+        if (_players.Count == 2)
+        {
+            throw new SimpleBlefException("For now only 2 players can play together in tournament");
+        }
+
+        if (_players.Contains(playerId))
+        {
+            throw new SimpleBlefException($"Player '{playerId}' already joined the tournament");
+        }
+
+        _players.Add(playerId);
     }
 }
