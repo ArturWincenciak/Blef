a2cdfafebc80904eea53924f1a9da4cb87a3901d
TeoVincent <artur.wincenciak@gmail.com>
2022-11-11 23:21:12 +0100

    feat #95: join game that is already started exception

---
+++ src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/JoinGameThatIsAlreadyStartedException.cs
@@ -0,0 +1,14 @@
+﻿using Blef.Shared.Kernel.Exceptions;
+
+namespace Blef.Modules.Games.Domain.Exceptions;
+
+internal sealed class JoinGameThatIsAlreadyStartedException : BlefException
+{
+    public JoinGameThatIsAlreadyStartedException(Guid gameId, Guid playerId)
+        : base(
+            title: "Cannot join game that is already started",
+            detail: $"Player '{playerId}' has tried to join game '{gameId}' that is already started",
+            instance: $"/games/{gameId}")
+    {
+    }
+}
