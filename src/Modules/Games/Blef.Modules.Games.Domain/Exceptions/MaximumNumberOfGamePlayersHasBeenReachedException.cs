e0f539171695058f382269185358522c86dd7d73
TeoVincent <artur.wincenciak@gmail.com>
2022-11-13 00:55:28 +0100

    feat #95: maximum number of tournament players has been reached exception

---
+++ src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/MaximumNumberOfGamePlayersHasBeenReachedException.cs
@@ -0,0 +1,25 @@
+﻿using Blef.Shared.Kernel.Exceptions;
+
+namespace Blef.Modules.Games.Domain.Exceptions;
+
+internal sealed class MaximumNumberOfGamePlayersHasBeenReachedException : BlefException
+{
+    public MaximumNumberOfGamePlayersHasBeenReachedException(Guid gameId)
+        : base(
+            title: "The maximum number of game players has been reached",
+            detail: "No more than 2 players can take part in the game",
+            instance: $"/game/{gameId}")
+    {
+    }
+}
+
+internal sealed class MaximumNumberOfTournamentPlayersHasBeenReachedException : BlefException
+{
+    public MaximumNumberOfTournamentPlayersHasBeenReachedException(Guid tournamentId)
+        : base(
+            title: "The maximum number of tournament players has been reached",
+            detail: "No more than 2 players can take part in the tournament",
+            instance: $"/tournaments/{tournamentId}")
+    {
+    }
+}
