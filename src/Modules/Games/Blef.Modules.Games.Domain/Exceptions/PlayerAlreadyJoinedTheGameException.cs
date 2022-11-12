49fc46fd6df69f706fdbb52f2a28d15d79a03223
TeoVincent <artur.wincenciak@gmail.com>
2022-11-12 22:40:02 +0100

    feat #95: player already joined the game exception

---
+++ src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/PlayerAlreadyJoinedTheGameException.cs
@@ -0,0 +1,14 @@
+﻿using Blef.Shared.Kernel.Exceptions;
+
+namespace Blef.Modules.Games.Domain.Exceptions;
+
+internal sealed class PlayerAlreadyJoinedTheGameException : BlefException
+{
+    public PlayerAlreadyJoinedTheGameException(Guid gameId, Guid playerId)
+        : base(
+            title: "Player already joined the game",
+            detail: $"Player '{playerId}' already joined the game",
+            instance: $"/game/{gameId}")
+    {
+    }
+}
