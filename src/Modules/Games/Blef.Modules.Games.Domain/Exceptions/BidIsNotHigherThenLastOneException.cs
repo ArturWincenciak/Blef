2d68d8dc475f5891de854c261f9f3ae6b9e77cac
TeoVincent <artur.wincenciak@gmail.com>
2022-11-12 22:48:35 +0100

    feat #95: the bid is not higher than last one exception

---
+++ src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/BidIsNotHigherThenLastOneException.cs
@@ -0,0 +1,14 @@
+﻿using Blef.Shared.Kernel.Exceptions;
+
+namespace Blef.Modules.Games.Domain.Exceptions;
+
+internal sealed class BidIsNotHigherThenLastOneException : BlefException
+{
+    public BidIsNotHigherThenLastOneException(Guid gameId, string newBid, string lastBid)
+        : base(
+            title: "The bid is not higher than last one",
+            detail: $"The new bid '{newBid}' is not higher than last one '{lastBid}'",
+            instance: $"/game/{gameId}")
+    {
+    }
+}
