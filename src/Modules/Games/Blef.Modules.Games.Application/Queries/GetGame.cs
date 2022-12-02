b52b9c8c2115e1e078cc3d238ffe895cf0239082
TeoVincent <artur.wincenciak@gmail.com>
2022-12-02 22:32:03 +0100

    Get game query/result record

---
+++ src/Modules/Games/Blef.Modules.Games.Application/Queries/GetGame.cs
@@ -0,0 +1,13 @@
+﻿using Blef.Shared.Abstractions.Queries;
+
+namespace Blef.Modules.Games.Application.Queries;
+
+public sealed record GetGame(Guid GameId) : IQuery<GetGame.Result>
+{
+    public sealed record Result(
+        PlayerBid[] Bids,
+        Guid CheckingPlayerId,
+        Guid LooserPlayerId) : IQueryResult;
+
+    public sealed record PlayerBid(Guid PlayerId, string Bid);
+}
