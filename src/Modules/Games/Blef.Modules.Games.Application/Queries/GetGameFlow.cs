af8a76b9c56db02b18522932b61ba9a4c0a4e6b6
TeoVincent <artur.wincenciak@gmail.com>
2022-12-03 00:54:27 +0100

    Get players cards in game flow query result

--- src/Modules/Games/Blef.Modules.Games.Application/Queries/GetGameFlow.cs
+++ src/Modules/Games/Blef.Modules.Games.Application/Queries/GetGameFlow.cs
@@ -11,5 +11,6 @@ public sealed record GetGameFlow(Guid GameId) : IQuery<GetGameFlow.Result>
         Guid LooserPlayerId) : IQueryResult;
 
     public sealed record GameBid(int Order, Guid PlayerId, string Bid);
-    public sealed record Player(Guid Id, string Nick);
+    public sealed record Player(Guid Id, string Nick, Card[] Cards);
+    public sealed record Card(string FaceCard, string Suit);
 }
