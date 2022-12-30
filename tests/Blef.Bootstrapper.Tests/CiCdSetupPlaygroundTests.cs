e93a86c18aba407888afb0829cee01ce1ec0bad1
TeoVincent <artur.wincenciak@gmail.com>
2022-12-30 22:07:29 +0100

    Cleanup: remove temp skipped test

--- tests/Blef.Bootstrapper.Tests/CiCdSetupPlaygroundTests.cs
+++ tests/Blef.Bootstrapper.Tests/CiCdSetupPlaygroundTests.cs
@@ -5,8 +5,4 @@ public class ContinuesIntegrationPlaygroundTests
     [Fact]
     public void AlwaysGreen() =>
         Assert.True(true);
-
-    [Fact(Skip = "Skip to test CI/CD")]
-    public void AlwaysRed() =>
-        Assert.True(false);
 }
