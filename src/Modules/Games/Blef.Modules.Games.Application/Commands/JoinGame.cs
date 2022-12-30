e62adfee32d2da0135963696f1c356c02f00b661
TeoVincent <artur.wincenciak@gmail.com>
2022-12-30 03:06:33 +0100

    Used implicitly attribute to ignore for usage-checking inspections

--- src/Modules/Games/Blef.Modules.Games.Application/Commands/JoinGame.cs
+++ src/Modules/Games/Blef.Modules.Games.Application/Commands/JoinGame.cs
@@ -1,8 +1,10 @@
 ﻿using Blef.Shared.Abstractions.Commands;
+using JetBrains.Annotations;
 
 namespace Blef.Modules.Games.Application.Commands;
 
 public sealed record JoinGame(Guid GameId, string Nick) : ICommand<JoinGame.Result>
 {
+    [UsedImplicitly]
     public sealed record Result(Guid PlayerId, string Nick) : ICommandResult;
 }
