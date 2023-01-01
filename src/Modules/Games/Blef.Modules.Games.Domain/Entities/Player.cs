6a35ae792708f778ae50441d7bcdd636781b7914
TeoVincent <artur.wincenciak@gmail.com>
2023-01-01 22:29:29 +0100

    Revert "Re-format code by JetBrains CleanupCode Tool"
    This reverts commit 785fb5486bf46d1287a43bf76ba5f689c7185ff4.

--- src/Modules/Games/Blef.Modules.Games.Domain/Entities/Player.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Entities/Player.cs
@@ -2,18 +2,18 @@ namespace Blef.Modules.Games.Domain.Entities;
 
 public class Player
 {
-    public string Nick { get; }
-
-    public Card[] DealtCards { get; }
-
-    public Guid Id { get; }
-
     private Player(Guid id, string nick, Card[] dealtCards)
     {
         Id = id;
         Nick = nick;
         DealtCards = dealtCards;
     }
+    
+    public string Nick { get; }
+
+    public Card[] DealtCards { get; }
+    
+    public Guid Id { get; }
 
     public static Player Create(string nick, Card[] dealtCards) =>
         new(id: Guid.NewGuid(), nick, dealtCards);
