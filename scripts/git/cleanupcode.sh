cd2265be592f6a718a9d817e0c1e80b4c29e237b
TeoVincent <artur.wincenciak@gmail.com>
2023-01-02 16:52:51 +0100

    Call cleanup code script with arguments

--- scripts/git/cleanupcode.sh
+++ scripts/git/cleanupcode.sh
@@ -1,15 +1,64 @@
 #!/bin/bash
 
-echo ""
-echo "--- --- ---"
-echo "Alright Cleanup Code Command-Line Tool"
-echo "--- --- ---"
-echo ""
+# Exit codes
+SUCCESS=0
+INVALID_ARGUMENT_ERROR=1
+EXIT_WITH_FAST_FAIL=2
+YOU_NEED_NO_CHANGES_BEFORE_RUN_CLEANUP_ERROR=3
+
+# Default arguments' values
+FAIL_ON_REFORMAT_NEEDED=no
+AUTO_COMMIT=yes
 
 echo ""
 echo "--- --- ---"
-echo "Fast fail: $1"
-echo "Auto commit: $2"
+echo "Alright Cleanup Code Command-Line Tool"
+echo "Default settings:"
+echo "- fail on re-format needed (-f): '$FAIL_ON_REFORMAT_NEEDED'"
+echo "- auto commit re-formated code (-a): '$AUTO_COMMIT'"
+echo "--- --- ---"
+echo ""
+
+while getopts f:a: flag
+do
+    case "${flag}" in
+        f) FAIL_ON_REFORMAT_NEEDED=${OPTARG};;
+        a) AUTO_COMMIT=${OPTARG};;
+    esac
+done
+
+if [ $FAIL_ON_REFORMAT_NEEDED != "yes" ] && [ $FAIL_ON_REFORMAT_NEEDED != "no" ]
+then
+	echo ""
+    echo "--- --- ---"
+    echo "INVALID ARGUMENT OF '-f' equals '$FAIL_ON_REFORMAT_NEEDED'"
+	echo "Set 'yes' or 'no' or omit to use default equals 'no'"
+    echo "--- --- ---"
+    echo ""
+	exit $INVALID_ARGUMENT_ERROR
+fi
+
+if [ $AUTO_COMMIT != "yes" ] && [ $AUTO_COMMIT != "no" ]
+then
+	echo ""
+    echo "--- --- ---"
+    echo "INVALID ARGUMENT OF '-a' equals '$AUTO_COMMIT'"
+	echo "Set 'yes' or 'no' or omit to use default equals 'no'"
+    echo "--- --- ---"
+    echo ""
+	exit $INVALID_ARGUMENT_ERROR
+fi
+
+echo ""
+echo "--- --- ---"
+echo "Your setup:"
+echo "- fail on re-format needed: $FAIL_ON_REFORMAT_NEEDED"
+echo "- auto commit re-formated code: $AUTO_COMMIT"
+if [ $FAIL_ON_REFORMAT_NEEDED == "yes" ] && [ $AUTO_COMMIT == "yes" ]; then
+	echo "NOTICE: you have set that the execution will fast fail on re-format needed"
+	echo "NOTICE: auto commit will not be executed because the execution will terminate with fail when re-format is needed"
+	echo "NOTICE: if you want to auto commit execute call the script with '-f no -a yes' arguments"
+fi
 echo "--- --- ---"
 echo ""
 
@@ -30,7 +79,7 @@ else
     echo ""
 
     git diff --name-only
-    exit 0
+    exit $YOU_NEED_NO_CHANGES_BEFORE_RUN_CLEANUP_ERROR
 fi
 
 STAGED_UNCOMMITTED=`git diff --staged --name-only`
@@ -38,19 +87,19 @@ if [ -z "$STAGED_UNCOMMITTED" ]
 then
     echo ""
     echo "--- --- ---"
-    echo "Right, there are no staged, uncommitted changes"
+    echo "Right, there is no any changes, repo is ready to cleanup"
     echo "--- --- ---"
     echo ""
 else
     echo ""
     echo "--- --- ---"
-    echo "There are no staged, uncommitted changes"
+    echo "There are staged, uncommitted changes"
     echo "Commit them before run the script"
     echo "--- --- ---"
     echo ""
 
     git diff --staged --name-only
-    exit 0
+    exit $YOU_NEED_NO_CHANGES_BEFORE_RUN_CLEANUP_ERROR
 fi
 
 echo ""
@@ -67,10 +116,30 @@ if [ -z "$REFORMATED_FILES" ]
 then
     echo ""
     echo "--- --- ---"
-    echo "No files re-formated"
+    echo "No files re-formated, everything is clean, congratulation!"
     echo "--- --- ---"
     echo ""
-    exit 0
+    exit $SUCCESS
+fi
+
+if [ $FAIL_ON_REFORMAT_NEEDED == "yes" ]
+then
+    echo ""
+    echo "--- --- ---"
+    echo "Exit with re-formated needed fail status"
+    echo "--- --- ---"
+    echo ""
+    exit $FAIL_ON_REFORMAT_NEEDED
+fi
+
+if [ $AUTO_COMMIT == "no" ]
+then
+    echo ""
+    echo "--- --- ---"
+    echo "There are re-formated files but thay will not be auto commited (as you set up)"
+    echo "--- --- ---"
+    echo ""
+    exit $SUCCESS
 fi
 
 echo ""
@@ -109,3 +178,10 @@ echo "--- --- ---"
 echo ""
 
 git status
+
+echo ""
+echo "--- --- ---"
+echo "All re-formated code has been commited with success"
+echo "--- --- ---"
+echo ""
+exit $SUCCESS
