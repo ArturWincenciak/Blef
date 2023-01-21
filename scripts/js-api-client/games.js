9d821e382291c8667321eced2f65b9aa47dc6bf6
TeoVincent <artur.wincenciak@gmail.com>
2023-01-21 20:06:09 +0100

    Example of API call by Java Script

---
+++ scripts/js-api-client/games.js
@@ -0,0 +1,21 @@
+﻿const Url = 'https://localhost:49153/games-module/games';
+const Request = {
+    headers: {
+        "content-type": "application/json; charset=UTF-8"
+    },
+    method: "POST"
+}
+
+const diplayResponse = data => {
+   if(!data) {
+       console.error("There is no data");
+       return;
+   }
+
+   console.info("Response: ", data);
+   document.getElementById('gameId').innerHTML = data.gameId;
+}
+
+fetch(Url, Request)
+    .then((response) => response.json())
+    .then((response) => diplayResponse(response));
