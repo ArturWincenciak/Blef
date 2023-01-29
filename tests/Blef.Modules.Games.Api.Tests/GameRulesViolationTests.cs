4a3e55d46a23926fd6b282df211cbcf11a5b2e55
TeoVincent <artur.wincenciak@gmail.com>
2023-01-29 12:08:58 +0100

    Game rules violation first test

---
+++ tests/Blef.Modules.Games.Api.Tests/GameRulesViolationTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Blef.Modules.Games.Api.Tests;
+
+public class GameRulesViolationTests
+{
+    [Fact]
+    public async Task CannotPlayWithYourself()
+    {
+        var exception = await Record.ExceptionAsync(async () =>
+        {
+            await new TestBuilder()
+                .NewGame()
+                .JoinPlayer(WhichPlayer.Knuth)
+                .Bid(WhichPlayer.Knuth, "one-of-a-kind:nine")
+                .Build();
+        });
+
+        Assert.NotNull(exception);
+    }
+}
