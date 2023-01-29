c9a238d3d49e9e85a55f350541f89e73a6598de8
TeoVincent <artur.wincenciak@gmail.com>
2023-01-30 00:09:21 +0100

    Test case with joining three players (red)

--- tests/Blef.Modules.Games.Api.Tests/ThreePlayersWithOneCardHappyPathGameTests.cs
+++ tests/Blef.Modules.Games.Api.Tests/ThreePlayersWithOneCardHappyPathGameTests.cs
@@ -1,6 +1,20 @@
+using Blef.Modules.Games.Api.Tests.Core;
+
 namespace Blef.Modules.Games.Api.Tests;
 
 public class ThreePlayersWithOneCardHappyPathGameTests
 {
-
+    [Fact]
+    public async Task OneBidEachAndCheck() =>
+        await new TestBuilder()
+            .NewGame()
+            .JoinPlayer(WhichPlayer.Knuth)
+            .JoinPlayer(WhichPlayer.Graham)
+            .JoinPlayer(WhichPlayer.Conway)
+            .Bid(WhichPlayer.Knuth, bid: "one-of-a-kind:nine")
+            .Bid(WhichPlayer.Graham, bid: "one-of-a-kind:ten")
+            .Bid(WhichPlayer.Graham, bid: "one-of-a-kind:jack")
+            .Check(WhichPlayer.Knuth)
+            .GetGameFlow()
+            .Build();
 }
