9f9402336dd03cdb4d4f26ce3ae6e1c5b642a0c8
TeoVincent <artur.wincenciak@gmail.com>
2023-01-29 21:48:40 +0100

    Test case: cannot join when game has been started

--- tests/Blef.Modules.Games.Api.Tests/GameRulesViolationTests.cs
+++ tests/Blef.Modules.Games.Api.Tests/GameRulesViolationTests.cs
@@ -18,4 +18,21 @@ public class GameRulesViolationTests
 
         Assert.NotNull(exception);
     }
+
+    [Fact]
+    public async Task CannotJoinWhenGameHasBeenStarted()
+    {
+        var exception = await Record.ExceptionAsync(async () =>
+        {
+            await new TestBuilder()
+                .NewGame()
+                .JoinPlayer(WhichPlayer.Knuth)
+                .JoinPlayer(WhichPlayer.Graham)
+                .Bid(WhichPlayer.Knuth, "one-of-a-kind:nine")
+                .JoinPlayer(WhichPlayer.Conway)
+                .Build();
+        });
+
+        Assert.NotNull(exception);
+    }
 }
