785351f929b7b501e979d962454439586f217eb3
TeoVincent <artur.wincenciak@gmail.com>
2023-02-02 23:01:55 +0100

    Cannot bid when game is already over

---
+++ tests/Blef.Modules.Games.Api.Tests/Core/Bid.cs
@@ -0,0 +1,11 @@
+﻿namespace Blef.Modules.Games.Api.Tests.Core;
+
+public static class Bid
+{
+    public static class OneOfAKind
+    {
+        public static string Nine = "one-of-a-kind:nine";
+        public static string Ten = "one-of-a-kind:ten";
+        public static string Jack = "one-of-a-kind:jack";
+    }
+}
