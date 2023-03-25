f13fc3ab2a8903ea10096bfd8ec95ffe50c7afe9
TeoVincent <artur.wincenciak@gmail.com>
2023-03-25 17:31:44 +0100

    Deal, Game, Player IDs value objects definitions

---
+++ src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/DealId.cs
@@ -0,0 +1,16 @@
+﻿namespace Blef.Modules.Games.Domain.ValueObjects;
+
+public sealed class DealId
+{
+    public DealId(GameId gameId, int number)
+    {
+        if (number < 1)
+            throw new ArgumentException("Deal number cannot be less than one");
+
+        GameId = gameId;
+        Number = number;
+    }
+
+    public GameId GameId { get; }
+    public int Number { get; }
+}
---
+++ src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/PlayerId.cs
@@ -0,0 +1,14 @@
+﻿namespace Blef.Modules.Games.Domain.ValueObjects;
+
+public sealed class PlayerId
+{
+    public PlayerId(Guid id)
+    {
+        if (id == Guid.Empty)
+            throw new AggregateException("Game player ID cannot be empty");
+
+        Id = id;
+    }
+
+    public Guid Id { get; }
+}
