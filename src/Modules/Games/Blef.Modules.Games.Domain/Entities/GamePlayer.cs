263fb43ff0e791896eae3f8124a4e14a16da7e3b
TeoVincent <artur.wincenciak@gmail.com>
2023-03-25 17:59:47 +0100

    Join game player

--- src/Modules/Games/Blef.Modules.Games.Domain/Entities/GamePlayer.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Entities/GamePlayer.cs
@@ -1,7 +1,18 @@
-﻿namespace Blef.Modules.Games.Domain.Entities;
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+
+namespace Blef.Modules.Games.Domain.Entities;
 
 public sealed class GamePlayer
 {
-    public Guid Id { get; }
+    public PlayerId Id { get; }
     public string Nick { get; }
+
+    private GamePlayer(PlayerId id, string nick)
+    {
+        Id = id;
+        Nick = nick;
+    }
+
+    public static GamePlayer Create(string nick) =>
+        new (new(Guid.NewGuid()), nick);
 }
