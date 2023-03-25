e4502bb2d34835669f03b116bbd07f22203babdf
TeoVincent <artur.wincenciak@gmail.com>
2023-03-25 17:31:44 +0100

    Deal, Game, Player IDs value objects definitions

---
+++ src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/GameId.cs
@@ -0,0 +1,14 @@
+﻿namespace Blef.Modules.Games.Domain.ValueObjects;
+
+public sealed class GameId
+{
+    public GameId(Guid id)
+    {
+        if (id == Guid.Empty)
+            throw new ArgumentException("Game ID cannot be empty");
+
+        Id = id;
+    }
+
+    public Guid Id { get; }
+}
