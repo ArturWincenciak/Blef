b461d5c7126bd8ad7748488e00300c827ae4e29a
TeoVincent <artur.wincenciak@gmail.com>
2023-03-25 17:21:07 +0100

    Delete old implementation

--- src/Modules/Games/Blef.Modules.Games.Domain/Entities/DealPlayer.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Entities/DealPlayer.cs
@@ -4,9 +4,5 @@ namespace Blef.Modules.Games.Domain.Entities;
 
 public sealed class DealPlayer
 {
-    public Guid Id { get; }
-    public string Nick { get; set; }
-    public int OrderNumber { get; }
-    public int CardsCount { get; }
-    public Card[] Cards { get; }
+
 }
--- src/Modules/Games/Blef.Modules.Games.Domain/Entities/Game.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Entities/Game.cs
@@ -1,21 +1,18 @@
 using Blef.Modules.Games.Domain.Exceptions;
-using Blef.Modules.Games.Domain.ValueObjects;
 
 namespace Blef.Modules.Games.Domain.Entities;
 
 public sealed class Game
 {
+    public Guid Id { get; }
+
     private const int MAX_NUMBER_OF_PLAYERS = 4;
     private const int MIN_NUMBER_OF_PLAYERS = 2;
 
-    private bool _isGameStarted;
-
     private readonly List<GamePlayer> _players = new();
-    private readonly List<Deal> _deals = new(); // todo
+    private readonly List<Deal> _deals = new();
 
-    public Guid Id { get; }
-
-    public Game(Guid id) =>
+    private Game(Guid id) =>
         Id = id;
 
     public static Game Create() =>
@@ -23,7 +20,8 @@ public sealed class Game
 
     public GamePlayer Join(string nick)
     {
-        if (_isGameStarted)
+        bool isGameStarted = _deals.Count > 0;
+        if (isGameStarted)
             throw new JoinGameThatIsAlreadyStartedException(Id, nick);
 
         if (_players.Count >= MAX_NUMBER_OF_PLAYERS)
@@ -36,16 +34,5 @@ public sealed class Game
         _players.Add(player);
 
         return player;
-    }
-
-    public (
-        IReadOnlyCollection<(Guid PlayerId, string Nick, Card[] Cards)> Players,
-        IReadOnlyCollection<(int Order, Guid PlayerId, string Bid)> Bids,
-        Guid CheckingPlayerId,
-        Guid LooserPlayerId
-        ) GetFlow()
-    {
-        // todo: get deals and forward the query to each deal
-        throw new NotImplementedException();
     }
 }
