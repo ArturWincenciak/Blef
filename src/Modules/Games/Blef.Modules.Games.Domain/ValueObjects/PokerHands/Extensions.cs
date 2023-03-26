452ba94cfabcc997f62fce75803f5f3b9b4a018d
TeoVincent <artur.wincenciak@gmail.com>
2023-03-26 23:43:51 +0200

    Pars poker hand in bid handler

--- src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/PokerHands/Extensions.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/PokerHands/Extensions.cs
@@ -4,6 +4,7 @@ namespace Blef.Modules.Games.Domain.ValueObjects.PokerHands;
 
 internal static class Extensions
 {
+    // todo: avoid this extension
     public static bool HasFaceCard(this IEnumerable<Card> cards, FaceCard faceCard) =>
         cards.Any(card => card.FaceCard == faceCard);
 }
