03889fadeafec28710319989ecac7b0dd1c227a6
TeoVincent <artur.wincenciak@gmail.com>
2023-04-07 21:22:54 +0200

    Remove not necessary integration tests code

--- tests/Blef.Modules.Games.Api.Tests/Core/ValueObjects/PlayerId.cs
+++ tests/Blef.Modules.Games.Api.Tests/Core/ValueObjects/PlayerId.cs
@@ -1,14 +1,3 @@
 ﻿namespace Blef.Modules.Games.Api.Tests.Core.ValueObjects;
 
-internal sealed record PlayerId
-{
-    public Guid Id { get; }
-
-    public PlayerId(Guid id)
-    {
-        if (id == Guid.Empty) // todo: exception
-            throw new Exception("TBD");
-
-        Id = id;
-    }
-}
+internal sealed record PlayerId(Guid Id);
