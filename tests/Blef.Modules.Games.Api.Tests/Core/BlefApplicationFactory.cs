10d0651017be65667f6bdb4725f19174f9848e25
TeoVincent <artur.wincenciak@gmail.com>
2023-04-08 13:08:30 +0200

    Mock deck factory for integration tests

--- tests/Blef.Modules.Games.Api.Tests/Core/BlefApplicationFactory.cs
+++ tests/Blef.Modules.Games.Api.Tests/Core/BlefApplicationFactory.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Blef.Modules.Games.Api.Tests.Scenarios.Services;
+using Blef.Modules.Games.Domain.Services;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Blef.Modules.Games.Api.Tests.Core;
 
-internal class BlefApplicationFactory : WebApplicationFactory<Program>
+internal sealed class BlefApplicationFactory : WebApplicationFactory<Program>
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder) =>
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.Replace(
+                ServiceDescriptor.Singleton<IDeckFactory>(
+                    _ => new DeckFactoryMock()));
+        });
+
         builder.UseEnvironment("test");
+    }
 }
---
+++ tests/Blef.Modules.Games.Api.Tests/Scenarios/Services/DeckFactoryMock.cs
@@ -0,0 +1,40 @@
+﻿using Blef.Modules.Games.Domain.Services;
+using Blef.Modules.Games.Domain.ValueObjects.Cards;
+
+namespace Blef.Modules.Games.Api.Tests.Scenarios.Services;
+
+internal sealed class DeckFactoryMock : IDeckFactory
+{
+    public Deck Create()
+    {
+        var cards = new []
+        {
+            new Card(FaceCard.Ace, Suit.Diamonds),
+            new Card(FaceCard.Ace, Suit.Spades),
+            new Card(FaceCard.Ten, Suit.Clubs),
+            new Card(FaceCard.Jack, Suit.Spades),
+            new Card(FaceCard.Queen, Suit.Diamonds),
+            new Card(FaceCard.King, Suit.Hearts),
+            new Card(FaceCard.King, Suit.Clubs),
+            new Card(FaceCard.Ace, Suit.Clubs),
+            new Card(FaceCard.Queen, Suit.Clubs),
+            new Card(FaceCard.Jack, Suit.Diamonds),
+            new Card(FaceCard.Ten, Suit.Diamonds),
+            new Card(FaceCard.King, Suit.Diamonds),
+            new Card(FaceCard.Nine, Suit.Clubs),
+            new Card(FaceCard.King, Suit.Spades),
+            new Card(FaceCard.Queen, Suit.Spades),
+            new Card(FaceCard.Jack, Suit.Clubs),
+            new Card(FaceCard.Nine, Suit.Spades),
+            new Card(FaceCard.Ace, Suit.Hearts),
+            new Card(FaceCard.Ten, Suit.Spades),
+            new Card(FaceCard.Ten, Suit.Hearts),
+            new Card(FaceCard.Nine, Suit.Hearts),
+            new Card(FaceCard.Nine, Suit.Diamonds),
+            new Card(FaceCard.Jack, Suit.Hearts),
+            new Card(FaceCard.Queen, Suit.Hearts)
+        };
+
+        return new Deck(cards);
+    }
+}
