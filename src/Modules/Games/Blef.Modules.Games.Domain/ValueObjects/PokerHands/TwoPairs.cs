424465705369cee0ede1df390e23fb98fbc0854a
TeoVincent <artur.wincenciak@gmail.com>
2023-04-08 17:53:50 +0200

    `Higher` and `Lower` instead `First` and `Second` in `TwoPairs` class

--- src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/PokerHands/TwoPairs.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/PokerHands/TwoPairs.cs
@@ -5,30 +5,37 @@ namespace Blef.Modules.Games.Domain.ValueObjects.PokerHands;
 internal sealed class TwoPairs : PokerHand
 {
     public const string Type = "two-pairs";
-    private readonly FaceCard _first;
-    private readonly FaceCard _second;
+    private readonly FaceCard _higher;
+    private readonly FaceCard _lower;
 
     protected override int PokerHandRank => 3;
 
     public TwoPairs(FaceCard first, FaceCard second)
     {
-        if (first <= second)
-            throw new ArgumentException($"First pair '{first}' has to be greater than second pair '{second}'");
+        if (first is FaceCard.None) // todo: exception
+            throw new ArgumentException("TOB");
 
-        _first = first;
-        _second = second;
+        if (second is FaceCard.None) // todo: exception
+            throw new ArgumentException("TBD");
+
+        if (first == second) // todo: exception
+            throw new ArgumentException("TBD");
+
+        (_higher, _lower) = first > second
+            ? (first, second)
+            : (second, first);
     }
 
     public override bool IsOnTable(Table table)
     {
-        var firstFaceCardCount = table.Count(_first);
-        var secondFaceCardCount = table.Count(_second);
-        return firstFaceCardCount >= 2 && secondFaceCardCount >= 2;
+        var higherFaceCardCount = table.Count(_higher);
+        var lowerFaceCardCount = table.Count(_lower);
+        return higherFaceCardCount >= 2 && lowerFaceCardCount >= 2;
     }
 
     protected override int GetInnerRank() =>
-        (10 * (int) _first) + (int) _second;
+        (10 * (int) _higher) + (int) _lower;
 
     public override string Serialize() =>
-        $"{Type}:{_first.ToString().ToLower()},{_second.ToString().ToLower()}";
+        $"{Type}:{_higher.ToString().ToLower()},{_lower.ToString().ToLower()}";
 }
