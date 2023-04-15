933df542d85f552a31d21c9d9281dd97f2eca797
TeoVincent <artur.wincenciak@gmail.com>
2023-04-15 22:08:24 +0200

    `PlayerNick` tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/PlayerNickTests.cs
@@ -0,0 +1,54 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class PlayerNickTests
+{
+    [Fact]
+    public void CreatePlayerNickTest()
+    {
+        var exception = Record.Exception(() => new PlayerNick("Kent"));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void NickCannotBeNullTest() =>
+        Assert.Throws<ArgumentException>(() => new PlayerNick(null));
+
+    [Fact]
+    public void NickCannotBeEmptyTest() =>
+        Assert.Throws<ArgumentException>(() => new PlayerNick(""));
+
+    [Fact]
+    public void NickCannotBeWhiteSpacesTest() =>
+        Assert.Throws<ArgumentException>(() => new PlayerNick("  "));
+
+    [Fact]
+    public void TwoInstancesOfPlayerNickWithTheSameNickAreEqualsTest()
+    {
+        // arrange
+        var nick = "Kent";
+        var first = new PlayerNick(nick);
+        var second = new PlayerNick(nick);
+
+        // act
+        var actual = first == second;
+
+        // assert
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void TwoInstancesOfPlayerNickWithDifferentNickAreNotEqualTest()
+    {
+        // arrange
+        var first = new PlayerNick("Kent");
+        var second = new PlayerNick("Graham");
+
+        // act
+        var actual = first != second;
+
+        // assert
+        Assert.True(actual);
+    }
+}
