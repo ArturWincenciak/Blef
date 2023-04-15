8d48a4d3968807f96b6ee9e57de01f99e5c209ba
TeoVincent <artur.wincenciak@gmail.com>
2023-04-15 23:42:48 +0200

    Cards amount tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/CardsAmountTests.cs
@@ -0,0 +1,88 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class CardsAmountTests
+{
+    [Fact]
+    public void InitialCardContainsOneCardTest()
+    {
+        // act
+        var cardAmount = CardsAmount.Initial;
+
+        // assert
+        Assert.Equal(expected: 1, actual: (int) cardAmount);
+    }
+
+    [Fact]
+    public void CanAddSeveralCardsTest()
+    {
+        // arrange
+        var cardAmount = CardsAmount.Initial;
+
+        // act
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+
+        // assert
+        Assert.Equal(expected: 4, actual: (int) cardAmount);
+    }
+
+    [Fact]
+    public void CannotExceedFiveCardsTest()
+    {
+        // arrange
+        var cardAmount = CardsAmount.Initial;
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+
+        // act, assert
+        Assert.Throws<InvalidOperationException>(() => cardAmount.AddOneCard());
+    }
+
+    [Fact]
+    public void InitialCardAmountIsLowerThenMaxCardAmountTest()
+    {
+        // act
+        var actual = CardsAmount.Initial < CardsAmount.Max;
+
+        // assert
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void FourIsLowerThenMaxTest()
+    {
+        // arrange
+        var cardAmount = CardsAmount.Initial;
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+
+        // act
+        var actual = cardAmount < CardsAmount.Max;
+
+        // assert
+        Assert.True(actual);
+    }
+
+    [Fact]
+    public void FiveIsNotLowerThenMaxTest()
+    {
+        // arrange
+        var cardAmount = CardsAmount.Initial;
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+        cardAmount.AddOneCard();
+
+        // act
+        var actual = cardAmount < CardsAmount.Max;
+
+        // assert
+        Assert.False(actual);
+    }
+}
---
+++ tests/Blef.Modules.Games.Domain.Tests/NextDealPlayerTests.cs
@@ -0,0 +1,41 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+using Blef.Modules.Games.Domain.ValueObjects.Ids;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class NextDealPlayerTests
+{
+    [Fact]
+    public void CreateNextDealPlayerTest()
+    {
+        // arrange
+        var playerId = new PlayerId(Guid.NewGuid());
+        var cardsAmount = CardsAmount.Initial;
+
+        // act
+        var exception = Record.Exception(() => new NextDealPlayer(playerId, cardsAmount));
+
+        // assert
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void CannotCreateWithNullArgumentsTest() =>
+        Assert.Throws<ArgumentNullException>(() => new NextDealPlayer(null, null));
+
+    [Fact]
+    public void CannotCreateWithNullPlayerIdArgumentTest() =>
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            var cardsAmount = CardsAmount.Initial;
+            return new NextDealPlayer(null, cardsAmount);
+        });
+
+    [Fact]
+    public void CannotCreateWithNullCardAmountArgumentTest() =>
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            var playerId = new PlayerId(Guid.NewGuid());
+            return new NextDealPlayer(playerId, null);
+        });
+}
