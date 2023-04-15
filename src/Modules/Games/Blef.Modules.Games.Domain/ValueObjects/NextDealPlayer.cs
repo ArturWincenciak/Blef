84b3589c342fe8c5730ac523cd9f08fa91b0f5ab
TeoVincent <artur.wincenciak@gmail.com>
2023-04-15 23:42:48 +0200

    Cards amount tests

--- src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/NextDealPlayer.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/NextDealPlayer.cs
@@ -2,4 +2,8 @@
 
 namespace Blef.Modules.Games.Domain.ValueObjects;
 
-internal sealed record NextDealPlayer(PlayerId PlayerId, CardsAmount CardsAmount);
+internal sealed record NextDealPlayer(PlayerId PlayerId, CardsAmount CardsAmount)
+{
+    public PlayerId PlayerId { get; } = PlayerId ?? throw new ArgumentNullException(nameof(PlayerId));
+    public CardsAmount CardsAmount { get; } = CardsAmount ?? throw new ArgumentNullException(nameof(CardsAmount));
+}
