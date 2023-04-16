a8298fd7faa72da9559176fb167d5e0c95eb35e5
TeoVincent <artur.wincenciak@gmail.com>
2023-04-16 14:35:45 +0200

    Checking player tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/CheckingPlayerTests.cs
@@ -0,0 +1,22 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class CheckingPlayerTests
+{
+    [Fact]
+    public void CreateCheckingPlayerTest()
+    {
+        var guid = Guid.Parse("F51D3D3C-BF45-4FE8-AA41-D07BF43D9A14");
+        var exception = Record.Exception(() => new CheckingPlayer(guid));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void CanCreateCheckingPlayerWithEmptyGuidTest()
+    {
+        var emptyGuid = Guid.Empty;
+        var exception = Record.Exception(() => new CheckingPlayer(emptyGuid));
+        Assert.Null(exception);
+    }
+}
