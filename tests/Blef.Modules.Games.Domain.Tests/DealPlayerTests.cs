4cd4703865018f2a696ce5ef9df24bbce1b742a4
TeoVincent <artur.wincenciak@gmail.com>
2023-04-16 14:31:56 +0200

    Deal player tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/DealPlayerTests.cs
@@ -0,0 +1,46 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+using Blef.Modules.Games.Domain.ValueObjects.Cards;
+using Blef.Modules.Games.Domain.ValueObjects.Ids;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class DealPlayerTests
+{
+    [Fact]
+    public void CreateDealPlayerTest()
+    {
+        var exception = Record.Exception(() =>
+        {
+            var guid = Guid.Parse("9D51E124-53F0-49A5-B1F8-F010AFB6F110");
+            var playerId = new PlayerId(guid);
+            var hand = new Hand(new[] {new Card(FaceCard.Ace, Suit.Clubs)});
+            return new DealPlayer(playerId, hand);
+        });
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void CannotCreateWithNullArgumentsTest() =>
+        Assert.Throws<ArgumentNullException>(() => new DealPlayer(null, null));
+
+    [Fact]
+    public void CannotCreateWithNullHandArgumentTest()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            var guid = Guid.Parse("BED79350-F2E8-4C5B-8B5C-E62C9B01E9D2");
+            var playerId = new PlayerId(guid);
+            return new DealPlayer(playerId, null);
+        });
+    }
+
+    [Fact]
+    public void CannotCreateWithNullPlayerIdArgumentTest()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            var hand = new Hand(new[] {new Card(FaceCard.Ace, Suit.Clubs)});
+            return new DealPlayer(null, hand);
+        });
+    }
+}
