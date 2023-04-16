9e1077eae3748c94af74eadd8204cb98394f984d
TeoVincent <artur.wincenciak@gmail.com>
2023-04-16 10:44:04 +0200

    Looser player tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/LooserPlayerTests.cs
@@ -0,0 +1,22 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class LooserPlayerTests
+{
+    [Fact]
+    public void CreateLooserPlayerTest()
+    {
+        var guid = Guid.NewGuid();
+        var exception = Record.Exception(() => new LooserPlayer(guid));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void CanCreateLooserPlayerWithEmptyGuidTest()
+    {
+        var emptyGuid = Guid.Empty;
+        var exception = Record.Exception(() => new LooserPlayer(emptyGuid));
+        Assert.Null(exception);
+    }
+}
