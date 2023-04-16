58a7c926e1d7749eb39e1a8a7f0c18fbe88cd932
TeoVincent <artur.wincenciak@gmail.com>
2023-04-16 23:06:39 +0200

    Table tests (WIP)

---
+++ tests/Blef.Modules.Games.Domain.Tests/TableTests.cs
@@ -0,0 +1,31 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects.Cards;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class TableTests
+{
+    [Fact]
+    public void CreateTableTest()
+    {
+        // arrange
+        var hands = new Hand[]
+        {
+            new(new Card[] { new(FaceCard.Ace, Suit.Clubs) }),
+            new(new Card[] { new(FaceCard.King, Suit.Hearts) }),
+            new(new Card[] { new(FaceCard.King, Suit.Clubs) })
+        };
+
+        // act
+        var actual = new Table(hands);
+
+        // assert
+        Assert.Equal(expected: 1, actual.Count(FaceCard.Ace));
+        Assert.Equal(expected: 2, actual.Count(FaceCard.King));
+        Assert.True(actual.Contains(FaceCard.Ace));
+        Assert.True(actual.Contains(FaceCard.King));
+        Assert.False(actual.Contains(FaceCard.Queen));
+        Assert.False(actual.Contains(FaceCard.Jack));
+        Assert.False(actual.Contains(FaceCard.Ten));
+        Assert.False(actual.Contains(FaceCard.Nine));
+    }
+}
