66bd95d59943aaeef280b264d502885b731e420c
TeoVincent <artur.wincenciak@gmail.com>
2023-04-16 15:18:00 +0200

    Deal number tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/DealNumberTests.cs
@@ -0,0 +1,21 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects.Ids;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class DealNumberTests
+{
+    [Fact]
+    public void CrateDealNumberTest()
+    {
+        var exception = Record.Exception(() =>
+        {
+            var dealNumber = new DealNumber(1);
+            Assert.Equal(expected: 1, actual: dealNumber.Number);
+        });
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void NumberCannotBeLessThanOneTest() =>
+        Assert.Throws<ArgumentException>(() => new DealNumber(0));
+}
