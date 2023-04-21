c7ad1089152a4d0217c0485793eef891e6a208a4
TeoVincent <artur.wincenciak@gmail.com>
2023-04-21 13:16:01 +0200

    Shuffled deck factory tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/ShuffledDeckFactoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Blef.Modules.Games.Domain.Services;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class ShuffledDeckFactoryTests
+{
+    [Fact]
+    public void CreateDeckByFactoryTest() =>
+        Assert.Null(Record.Exception(() =>
+        {
+            var factory = new ShuffledDeckFactory();
+            return factory.Create();
+        }));
+}
