cf4d1757ae56cc3b8adf39e02e0a6797b973f6f6
TeoVincent <artur.wincenciak@gmail.com>
2023-04-21 22:34:31 +0200

    Contract and domain exceptions of deal

--- src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/BidIsNotHigherThenLastOneException.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/BidIsNotHigherThenLastOneException.cs
@@ -1,15 +1,16 @@
-﻿using Blef.Modules.Games.Domain.ValueObjects.Ids;
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+using Blef.Modules.Games.Domain.ValueObjects.Ids;
 using Blef.Shared.Kernel.Exceptions;
 
 namespace Blef.Modules.Games.Domain.Exceptions;
 
 internal sealed class BidIsNotHigherThenLastOneException : BlefException
 {
-    public BidIsNotHigherThenLastOneException(GameId gameId, string newBid, string lastBid)
+    public BidIsNotHigherThenLastOneException(DealId dealId, Bid newBid, Bid lastBid)
         : base(
             title: "The bid is not higher than last one",
             detail: $"The new bid '{newBid}' is not higher than last one '{lastBid}'",
-            instance: $"/games/{gameId}")
+            instance: $"/games/{dealId.GameId}/deals/{dealId.Number}")
     {
     }
 }
