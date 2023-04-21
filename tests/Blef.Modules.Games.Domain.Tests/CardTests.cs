660cbf616ee5510d65f889b3365ac9bb95f6f3a3
TeoVincent <artur.wincenciak@gmail.com>
2023-04-21 12:15:36 +0200

    Card tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/CardTests.cs
@@ -0,0 +1,29 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects.Cards;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class CardTests
+{
+    [Fact]
+    public void CreateCardTest()
+    {
+        // act
+        var actual = new Card(FaceCard.Ace, Suit.Clubs);
+
+        // assert
+        Assert.True(FaceCard.Ace == actual.FaceCard);
+        Assert.Equal(expected: Suit.Clubs, actual.Suit);
+    }
+
+    [Fact]
+    public void CannotCreateWithNullArgumentsTest() =>
+        Assert.Throws<ArgumentNullException>(() => new Card(null, null));
+
+    [Fact]
+    public void CannotCreateWithNullFaceCardArgumentTest() =>
+        Assert.Throws<ArgumentNullException>(() => new Card(null, Suit.Clubs));
+
+    [Fact]
+    public void CannotCreateWithNullSuitArgumentTest() =>
+        Assert.Throws<ArgumentNullException>(() => new Card(FaceCard.Ace, null));
+}
