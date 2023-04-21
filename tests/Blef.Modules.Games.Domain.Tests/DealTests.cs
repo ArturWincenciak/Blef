62eca32d5b18a161ef1c0ca0cf48bdcb9485b1d1
TeoVincent <artur.wincenciak@gmail.com>
2023-04-21 22:54:52 +0200

    Deal tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/DealTests.cs
@@ -0,0 +1,36 @@
+﻿using Blef.Modules.Games.Domain.Entities;
+using Blef.Modules.Games.Domain.ValueObjects;
+using Blef.Modules.Games.Domain.ValueObjects.Cards;
+using Blef.Modules.Games.Domain.ValueObjects.Ids;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class DealTests
+{
+    [Fact]
+    public void CreateDealTest()
+    {
+        // arrange
+        var dealNumber = new DealNumber(1);
+        var gameGuid = Guid.Parse("9A7A3E9F-823C-4496-8356-08745BDE7EF6");
+        var gameId = new GameId(gameGuid);
+        var dealId = new DealId(gameId, dealNumber);
+        var playerGuid_1 = Guid.Parse("A034FDB8-C4CF-479E-B376-49C595560F9F");
+        var playerGuid_2 = Guid.Parse("05E901E3-E4BD-4904-852A-C31B15A49F88");
+        var playerHand_1 = new Hand(new Card[] {new(FaceCard.Ace, Suit.Clubs)});
+        var playerHand_2 = new Hand(new Card[] {new(FaceCard.King, Suit.Diamonds)});
+        var players = new DealPlayer[]
+        {
+            new(new(playerGuid_1), playerHand_1),
+            new(new(playerGuid_2), playerHand_2)
+        };
+
+        // act
+        var actual = new Deal(dealId, players);
+
+        // assert
+        Assert.Equal(new DealId(new GameId(gameGuid), new DealNumber(1)), actual.DealId);
+        Assert.Equal(playerHand_1, actual.GetHand(new PlayerId(playerGuid_1)));
+        Assert.Equal(playerHand_2, actual.GetHand(new PlayerId(playerGuid_2)));
+    }
+}
