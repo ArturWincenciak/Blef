9dc0da64027c7ff3cc46b7510924c224472ab8e6
TeoVincent <artur.wincenciak@gmail.com>
2023-04-22 10:35:00 +0200

    Calculate next deal order (WIP)

--- src/Modules/Games/Blef.Modules.Games.Domain/Entities/GamePlayer.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Entities/GamePlayer.cs
@@ -9,18 +9,20 @@ internal sealed class GamePlayer
     public PlayerId PlayerId { get; }
     public PlayerNick Nick { get; }
     public CardsAmount CardsAmount { get; private set; }
+    public int JoiningSequence { get; }
     public bool IsInTheGame => !_isOutOfTheGame;
 
-    private GamePlayer(PlayerId playerId, PlayerNick nick, CardsAmount cardsAmount)
+    private GamePlayer(PlayerId playerId, PlayerNick nick, CardsAmount cardsAmount, int joiningSequence)
     {
         PlayerId = playerId ?? throw new ArgumentNullException(nameof(playerId));
         Nick = nick ?? throw new ArgumentNullException(nameof(nick));
         CardsAmount = cardsAmount ?? throw new ArgumentNullException(nameof(cardsAmount));
         _isOutOfTheGame = false;
+        JoiningSequence = joiningSequence;
     }
 
-    public static GamePlayer Create(PlayerNick nick) =>
-        new(playerId: new PlayerId(Guid.NewGuid()), nick, CardsAmount.Initial);
+    public static GamePlayer Create(PlayerNick nick, int joiningSequence) =>
+        new(playerId: new PlayerId(Guid.NewGuid()), nick, CardsAmount.Initial, joiningSequence);
 
     public void LostLastDeal()
     {
