4b59ef6e346fcac13f880b28e89aa0339cb3b7d3
TeoVincent <artur.wincenciak@gmail.com>
2023-04-28 22:31:29 +0200

    Order tests

---
+++ tests/Blef.Modules.Games.Domain.Tests/OrderTests.cs
@@ -0,0 +1,26 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class OrderTests
+{
+    [Fact]
+    public void CreateOrderTest()
+    {
+        // act
+        var exception = Record.Exception(() =>
+        {
+            Order.Create(sequence: 1);
+            Order.Create(sequence: 2);
+            Order.Create(sequence: 3);
+            Order.Create(sequence: 4);
+        });
+
+        // assert
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void FirstPropertyEqualsCreatedFirstTest() =>
+        Assert.True(Order.First == Order.Create(1));
+}
