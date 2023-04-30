60d90702c18e64c6b40b4215c2f445e989bfe700
TeoVincent <artur.wincenciak@gmail.com>
2023-04-30 11:26:40 +0200

    Clean up code

--- src/Modules/Games/Blef.Modules.Games.Domain/Services/MoveOrderPolicy.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Services/MoveOrderPolicy.cs
@@ -1,7 +1,7 @@
 ﻿using Blef.Modules.Games.Domain.ValueObjects;
 using Blef.Modules.Games.Domain.ValueObjects.Ids;
 
-namespace Blef.Modules.Games.Domain.Entities;
+namespace Blef.Modules.Games.Domain.Services;
 
 internal sealed class MoveOrderPolicy
 {
---
+++ src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/Move.cs
@@ -0,0 +1,6 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects.Ids;
+
+namespace Blef.Modules.Games.Domain.ValueObjects;
+
+// todo: validation
+internal sealed record Move(PlayerId Player, Order Order);
