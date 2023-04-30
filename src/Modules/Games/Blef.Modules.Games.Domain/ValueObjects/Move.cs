5682cb01fed1bb2fd3ccdf5267e0fcb06bc1b39f
TeoVincent <artur.wincenciak@gmail.com>
2023-04-30 13:16:51 +0200

    Move tests

--- src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/Move.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/Move.cs
@@ -2,6 +2,8 @@
 
 namespace Blef.Modules.Games.Domain.ValueObjects;
 
-// todo: validation
-// todo: test
-internal sealed record Move(PlayerId Player, Order Order);
+internal sealed record Move(PlayerId Player, Order Order)
+{
+    public PlayerId Player { get; } = Player ?? throw new ArgumentNullException(nameof(Player));
+    public Order Order { get; } = Order ?? throw new ArgumentNullException(nameof(Order));
+}
---
+++ tests/Blef.Modules.Games.Domain.Tests/MoveTests.cs
@@ -0,0 +1,32 @@
+﻿using Blef.Modules.Games.Domain.ValueObjects;
+using Blef.Modules.Games.Domain.ValueObjects.Ids;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class MoveTests
+{
+    [Fact]
+    public void CreateMoveTest()
+    {
+        // arrange
+        var player = new PlayerId(Guid.Parse("89FAAC08-D5A9-4611-98D4-EBDEA0749E67"));
+        var order = Order.Create(sequence: 1);
+
+        // act
+        var exception = Record.Exception(() => new Move(player, order));
+
+        // assert
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void CannotCreateMoveWithNullPlayerTest() =>
+        Assert.Throws<ArgumentNullException>(() => new Move(Player: null, Order.First));
+
+    [Fact]
+    public void CannotCreateMoveWithNullOrderTest() =>
+        Assert.Throws<ArgumentNullException>(() => new Move(
+            new PlayerId(Guid.Parse("A2C87FD6-C1C4-4B6D-9CBD-CE41265050EA")),
+            Order: null));
+
+}
