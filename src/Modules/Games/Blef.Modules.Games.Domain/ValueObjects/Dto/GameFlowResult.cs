66970e1c17869d69eecd1de5dd977c72a45abb7f
TeoVincent <artur.wincenciak@gmail.com>
2023-05-03 00:20:09 +0200

    Remove old version of get deal flow query handler

--- src/Modules/Games/Blef.Modules.Games.Domain/ValueObjects/Dto/GameFlowResult.cs
+++
@@ -1,6 +0,0 @@
-﻿using Blef.Modules.Games.Domain.Entities;
-using Blef.Modules.Games.Domain.ValueObjects.Ids;
-
-namespace Blef.Modules.Games.Domain.ValueObjects.Dto;
-
-internal record GameFlowResult(IEnumerable<GamePlayer> Players, IEnumerable<DealId> Deals);
