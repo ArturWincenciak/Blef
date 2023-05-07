0439e0245864f8e220882c9465fcdf53386a75fc
TeoVincent <artur.wincenciak@gmail.com>
2023-05-07 15:16:38 +0200

    Create next deal players with deal order physic

---
+++ src/Modules/Games/Blef.Modules.Games.Domain/Model/DealOrderPhysic.cs
@@ -0,0 +1,21 @@
+﻿namespace Blef.Modules.Games.Domain.Model;
+
+internal sealed class DealOrderPhysic
+{
+    private readonly int _playersCount;
+    private int shift;
+
+    public DealOrderPhysic(int playersCount) =>
+        _playersCount = playersCount;
+
+    public int GetOrder(int baseSequence)
+    {
+        if (baseSequence - shift <= 0)
+            return _playersCount + (baseSequence - shift);
+
+        return baseSequence - shift;
+    }
+
+    public void Move(int sequenceShift) =>
+        shift = sequenceShift % _playersCount;
+}
---
+++ tests/Blef.Modules.Games.Domain.Tests/DealOrderPhysicTests.cs
@@ -0,0 +1,75 @@
+﻿using Blef.Modules.Games.Domain.Model;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class DealOrderPhysicTests
+{
+    [Fact]
+    public void Test1()
+    {
+        // arrange
+        var orderPhysic = new DealOrderPhysic(4);
+
+        // act, assert
+        Assert.Equal(1, orderPhysic.GetOrder(1));
+        Assert.Equal(2, orderPhysic.GetOrder(2));
+        Assert.Equal(3, orderPhysic.GetOrder(3));
+        Assert.Equal(4, orderPhysic.GetOrder(4));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // arrange
+        var orderPhysic = new DealOrderPhysic(4);
+        orderPhysic.Move(1);
+
+        // act, assert
+        Assert.Equal(4, orderPhysic.GetOrder(1));
+        Assert.Equal(1, orderPhysic.GetOrder(2));
+        Assert.Equal(2, orderPhysic.GetOrder(3));
+        Assert.Equal(3, orderPhysic.GetOrder(4));
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        // arrange
+        var orderPhysic = new DealOrderPhysic(4);
+        orderPhysic.Move(2);
+
+        // act, assert
+        Assert.Equal(3, orderPhysic.GetOrder(1));
+        Assert.Equal(4, orderPhysic.GetOrder(2));
+        Assert.Equal(1, orderPhysic.GetOrder(3));
+        Assert.Equal(2, orderPhysic.GetOrder(4));
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        // arrange
+        var orderPhysic = new DealOrderPhysic(4);
+        orderPhysic.Move(3);
+
+        // act, assert
+        Assert.Equal(2, orderPhysic.GetOrder(1));
+        Assert.Equal(3, orderPhysic.GetOrder(2));
+        Assert.Equal(4, orderPhysic.GetOrder(3));
+        Assert.Equal(1, orderPhysic.GetOrder(4));
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        // arrange
+        var orderPhysic = new DealOrderPhysic(4);
+        orderPhysic.Move(4);
+
+        // act, assert
+        Assert.Equal(1, orderPhysic.GetOrder(1));
+        Assert.Equal(2, orderPhysic.GetOrder(2));
+        Assert.Equal(3, orderPhysic.GetOrder(3));
+        Assert.Equal(4, orderPhysic.GetOrder(4));
+    }
+}
