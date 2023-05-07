72966761cee538816cfdd820efe3f012f1d3d62e
TeoVincent <artur.wincenciak@gmail.com>
2023-05-07 16:09:31 +0200

    Adjust player lost game tests

--- tests/Blef.Modules.Games.Domain.Tests/Extensions/AssertExtension.cs
+++ tests/Blef.Modules.Games.Domain.Tests/Extensions/AssertExtension.cs
@@ -29,6 +29,7 @@ internal static class AssertExtension
         var dealStarted = actual.Single(@event => @event is DealStarted) as DealStarted;
         Assert.Equal(expectedGameId, dealStarted!.Game);
         Assert.Equal(expectedDealNumber, dealStarted.Deal);
+        Assert.Equal(expectedNextDealPlayers.Count(), dealStarted.Players.Count());
 
         var nextDealPlayers = dealStarted.Players.Select(dealPlayer => dealPlayer.Player);
         foreach (var expectedNextDealPlayer in expectedNextDealPlayers)
