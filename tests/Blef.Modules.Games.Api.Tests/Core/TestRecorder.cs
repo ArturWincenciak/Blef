3a8c30bea5bc3775481e78ac25bbd5fba8d210bd
TeoVincent <artur.wincenciak@gmail.com>
2023-05-14 22:56:53 +0200

    [NotAccessedPositionalProperty.Global] Positional properties is never accessed (except in implicit Equals/ToString implementations)

--- tests/Blef.Modules.Games.Api.Tests/Core/TestRecorder.cs
+++ tests/Blef.Modules.Games.Api.Tests/Core/TestRecorder.cs
@@ -1,5 +1,6 @@
 ﻿using Blef.Shared.Abstractions.Commands;
 using Blef.Shared.Abstractions.Queries;
+using JetBrains.Annotations;
 
 namespace Blef.Modules.Games.Api.Tests.Core;
 
@@ -16,7 +17,9 @@ internal sealed class TestRecorder
     public void Record(string action, object argument, ICommandResult result) =>
         _items.Add(new TestResult(++_counter, action, argument, result));
 
+    [UsedImplicitly]
     internal record TestResult(int No, string Action, object Argument, object Result);
 
+    [UsedImplicitly]
     internal record EmptyCommandResult(string Status) : ICommandResult;
 }
