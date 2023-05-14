94a87aa100ee80144c50d06de6e4342564f88c6e
TeoVincent <artur.wincenciak@gmail.com>
2023-05-14 22:59:46 +0200

    [ConvertToConstant.Global] Convert into constant

--- tests/Blef.Modules.Games.Api.Tests/Scenarios/ValueObjects/PokerHand.cs
+++ tests/Blef.Modules.Games.Api.Tests/Scenarios/ValueObjects/PokerHand.cs
@@ -4,8 +4,8 @@ public static class PokerHand
 {
     public static class HighCard
     {
-        public readonly static string Nine = "high-card:nine";
-        public readonly static string Ten = "high-card:ten";
-        public readonly static string Jack = "high-card:jack";
+        public const string NINE = "high-card:nine";
+        public const string TEN = "high-card:ten";
+        public const string JACK = "high-card:jack";
     }
 }
