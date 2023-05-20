43fa0140edd2402dfa5ca95ab337827826ad855d
TeoVincent <artur.wincenciak@gmail.com>
2023-05-20 14:31:07 +0200

    Clean up code

--- src/Shared/Blef.Shared.Abstractions/Events/IDomainEventHandler.cs
+++ src/Shared/Blef.Shared.Abstractions/Events/IDomainEventHandler.cs
@@ -1,7 +1,10 @@
-﻿namespace Blef.Shared.Abstractions.Events;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Blef.Shared.Abstractions.Events;
 
 public interface IDomainEventHandler<in TEvent>
     where TEvent : IDomainEvent
 {
+    [SuppressMessage("ReSharper", "UnusedParameter.Global")]
     Task Handle(TEvent @event, CancellationToken cancellation);
 }
