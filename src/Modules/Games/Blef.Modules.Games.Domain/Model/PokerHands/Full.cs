5a0d2acb4e6cd287414e96d0aaabaea8d9310e9f
TeoVincent <artur.wincenciak@gmail.com>
2023-05-21 10:43:40 +0200

    Full poker hand implementation

---
+++ src/Modules/Games/Blef.Modules.Games.Domain/Model/PokerHands/Full.cs
@@ -0,0 +1,36 @@
+﻿namespace Blef.Modules.Games.Domain.Model.PokerHands;
+
+internal sealed class Full : PokerHand
+{
+    public const string TYPE = "full";
+    private readonly FaceCard _threeOfAKind;
+    private readonly FaceCard _pair;
+
+    protected override int PokerHandRank => 7;
+
+    private Full(FaceCard threeOfAKind, FaceCard pair)
+    {
+        if(threeOfAKind.Equals(pair))
+            throw new ArgumentException("Three of a kind and pair cannot be the same card");
+
+        _threeOfAKind = threeOfAKind;
+        _pair = pair;
+    }
+
+    public override bool IsOnTable(Table table) =>
+        table.Count(_threeOfAKind) >= 3 && table.Count(_pair) >= 2;
+
+    protected override int GetInnerRank() =>
+        (10 * _threeOfAKind.GetRank()) + _pair.GetRank();
+
+    public override string Serialize() =>
+        $"{TYPE}:{_threeOfAKind.ToString().ToLower()},{_pair.ToString().ToLower()}";
+
+    public static PokerHand Create(string faceCards)
+    {
+        var faceCardParts = faceCards.Split(",");
+        return new Full(
+            threeOfAKind: FaceCard.Create(faceCardParts[0]),
+            pair: FaceCard.Create(faceCardParts[1]));
+    }
+}
