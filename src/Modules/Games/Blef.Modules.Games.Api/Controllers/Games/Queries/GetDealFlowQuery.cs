66a4d4d239ff92bcb1dd6342bebdd7fe98a815fa
TeoVincent <artur.wincenciak@gmail.com>
2023-06-11 23:02:03 +0200

    Feat: validate `Gameplay` endpoints

---
+++ src/Modules/Games/Blef.Modules.Games.Api/Controllers/Games/Queries/GetDealFlowQuery.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Blef.Shared.Infrastructure.Api.Validation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blef.Modules.Games.Api.Controllers.Games.Queries;
+
+public record GetDealFlowQuery(
+    [FromRoute] [NotEmptyGuid] Guid GameId,
+    [FromRoute] [Range(minimum: 1, Int32.MaxValue)] int DealNumber)
+{
+    public const string ROUTE = $"{{{nameof(GameId)}:Guid}}/deals/{{{nameof(DealNumber)}:int}}";
+}
