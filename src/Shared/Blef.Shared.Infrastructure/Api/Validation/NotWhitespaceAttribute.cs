4e7d3a56db6ebab917ae5665a12de61baab7a4b4
TeoVincent <artur.wincenciak@gmail.com>
2023-06-11 23:49:40 +0200

    Feat: not white space string validate attribute

---
+++ src/Shared/Blef.Shared.Infrastructure/Api/Validation/NotWhitespaceAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blef.Shared.Infrastructure.Api.Validation;
+
+public class NotWhitespaceAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is null)
+            return new ValidationResult("Value cannot be null.");
+
+        if (value is not string stringValue)
+            return new ValidationResult("Value is not a string.");
+
+        if (string.IsNullOrWhiteSpace(stringValue))
+            return new ValidationResult("String value cannot be empty or whitespace.");
+
+        return ValidationResult.Success;
+    }
+}
