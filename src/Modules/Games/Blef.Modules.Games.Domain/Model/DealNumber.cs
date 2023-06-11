260adb0d51f380cc3652df0da93bc88347e0d179
TeoVincent <artur.wincenciak@gmail.com>
2023-06-12 01:17:34 +0200

    Refactor: make domain model internal

--- src/Modules/Games/Blef.Modules.Games.Domain/Model/DealNumber.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Model/DealNumber.cs
@@ -1,7 +1,6 @@
 ﻿namespace Blef.Modules.Games.Domain.Model;
 
-// todo: change to internal
-public sealed record DealNumber(int Number)
+internal sealed record DealNumber(int Number)
 {
     public int Number { get; } = Number < 1
         ? throw new ArgumentException("Deal number cannot be less than one")
--- src/Modules/Games/Blef.Modules.Games.Domain/Model/GameId.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Model/GameId.cs
@@ -1,7 +1,6 @@
 ﻿namespace Blef.Modules.Games.Domain.Model;
 
-// todo: change to internal
-public sealed record GameId(Guid Id)
+internal sealed record GameId(Guid Id)
 {
     public Guid Id { get; } = Id == Guid.Empty
         ? throw new ArgumentException("Game ID cannot be empty")
