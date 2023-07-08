b9c9c1c2400ed49640acc18c3dc18c8ee113c63d
TeoVincent <artur.wincenciak@gmail.com>
2023-07-08 07:35:48 +0200

    Notice: "[UnusedMember.Global] Enum member 'Nine|Ten|...' is never used" on FaceCard.cs

--- src/Modules/Games/Blef.Modules.Games.Api/Controllers/Games/Commands/Suit.cs
+++ src/Modules/Games/Blef.Modules.Games.Api/Controllers/Games/Commands/Suit.cs
@@ -1,9 +1,18 @@
-﻿namespace Blef.Modules.Games.Api.Controllers.Games.Commands;
+﻿using JetBrains.Annotations;
+
+namespace Blef.Modules.Games.Api.Controllers.Games.Commands;
 
 internal enum Suit
 {
+    [UsedImplicitly]
     Clubs = 1,
+
+    [UsedImplicitly]
     Diamonds = 2,
+
+    [UsedImplicitly]
     Hearts = 4,
+
+    [UsedImplicitly]
     Spades = 8
 }
