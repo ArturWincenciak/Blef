7fc0125027b8d75e36c6eaf500b2dcbfe4f5d674
TeoVincent <artur.wincenciak@gmail.com>
2023-07-08 07:27:47 +0200

    Warning: "[NotAccessedPositionalProperty.Global] Positional property 'FullHouseBidsRoute.FaceCard' is never accessed

--- src/Modules/Games/Blef.Modules.Games.Api/Controllers/Games/Commands/FullHouseBidsRoute.cs
+++ src/Modules/Games/Blef.Modules.Games.Api/Controllers/Games/Commands/FullHouseBidsRoute.cs
@@ -1,6 +1,6 @@
 ﻿namespace Blef.Modules.Games.Api.Controllers.Games.Commands;
 
-internal sealed record FullHouseBidsRoute(Guid GameId, Guid PlayerId, FaceCard FaceCard) : BidsRoute(GameId, PlayerId)
+internal sealed record FullHouseBidsRoute(Guid GameId, Guid PlayerId) : BidsRoute(GameId, PlayerId)
 {
     public const string ROUTE = $"{BASE_ROUTE}/full-house";
 }
