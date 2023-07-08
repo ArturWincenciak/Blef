a67effe1eb96db2a8621cf2281fcfd29b0d07534
TeoVincent <artur.wincenciak@gmail.com>
2023-07-08 20:55:34 +0200

    Tests: that is not this player turn now exception

--- src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/ThatIsNotThisPlayerTurnNowException.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/ThatIsNotThisPlayerTurnNowException.cs
@@ -5,11 +5,11 @@ namespace Blef.Modules.Games.Domain.Exceptions;
 
 internal sealed class ThatIsNotThisPlayerTurnNowException : BlefException
 {
-    public ThatIsNotThisPlayerTurnNowException(PlayerId playerId)
+    public ThatIsNotThisPlayerTurnNowException(PlayerId playerId, string instance)
         : base(
             title: "That is not this player's turn now",
             detail: $"Player '{playerId}' should wait for his turn",
-            string.Empty)
+            instance)
     {
     }
 }
