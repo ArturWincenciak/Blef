dea116b228f259bd396bb0b01f8e63c541347b61
TeoVincent <artur.wincenciak@gmail.com>
2023-07-08 08:07:54 +0200

    Notice: "[UnusedMember.Global] Enum member 'Hearts' is never used" on Suit.cs

--- tests/Blef.Modules.Games.Api.Tests/Scenarios/ValueObjects/Suit.cs
+++ tests/Blef.Modules.Games.Api.Tests/Scenarios/ValueObjects/Suit.cs
@@ -1,9 +1,12 @@
-﻿namespace Blef.Modules.Games.Api.Tests.Scenarios.ValueObjects;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Blef.Modules.Games.Api.Tests.Scenarios.ValueObjects;
 
 internal enum Suit
 {
     Clubs = 1,
     Diamonds = 2,
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     Hearts = 4,
     Spades = 8,
     NotValidValue = 16
