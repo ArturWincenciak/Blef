ce62627aaea9aa163c64265833ab5acdda1aa31a
TeoVincent <artur.wincenciak@gmail.com>
2023-07-26 21:34:41 +0200

    todo: create strict object of PokerHand instead of string

--- src/Modules/Games/Blef.Modules.Games.Application/Queries/GetDeal.cs
+++ src/Modules/Games/Blef.Modules.Games.Application/Queries/GetDeal.cs
@@ -11,6 +11,8 @@ public sealed record GetDeal(Guid GameId, int DealNumber) : IQuery<GetDeal.Resul
         IReadOnlyCollection<Bid> Bids,
         DealResolution DealResolution) : IQueryResult;
 
+    // todo: create strict object of PokerHand instead of string
+    // currently the poker hand for instance can return value like that: "PokerHand: two-pairs:ten,nine"
     [UsedImplicitly]
     public sealed record Bid(int Order, Guid PlayerId, string PokerHand);
 
