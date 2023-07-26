ad6b8261e7fdd654da0546f24c634eb1376b5fbe
TeoVincent <artur.wincenciak@gmail.com>
2023-07-26 23:21:43 +0200

    Tests: two players play the game

--- tests/Blef.Modules.Games.Api.Tests/Scenarios/HappyPath/HappyPathTests.cs
+++ tests/Blef.Modules.Games.Api.Tests/Scenarios/HappyPath/HappyPathTests.cs
@@ -6,7 +6,6 @@ namespace Blef.Modules.Games.Api.Tests.Scenarios.HappyPath;
 [UsesVerify]
 public class HappyPathTests
 {
-    // todo: play all game with two players
     [Fact]
     public async Task TwoPlayersPlayTheGameTest()
     {
@@ -16,28 +15,49 @@ public class HappyPathTests
             .JoinPlayer(WhichPlayer.Knuth)
             .JoinPlayer(WhichPlayer.Graham)
             .NewDeal()
+            .GetGameFlow()
+            .GetDealFlow(new DealNumber(1))
             
+            .GetCards(WhichPlayer.Knuth, new DealNumber(1))
+            .GetCards(WhichPlayer.Graham, new DealNumber(1))
             .BidHighCard(WhichPlayer.Knuth, FaceCard.Nine)
             .Check(WhichPlayer.Graham)
             .GetGameFlow()
             .GetDealFlow(new DealNumber(1))
             
+            .GetCards(WhichPlayer.Knuth, new DealNumber(2))
+            .GetCards(WhichPlayer.Graham, new DealNumber(2))
             .BidHighCard(WhichPlayer.Graham, FaceCard.Ace)
             .Check(WhichPlayer.Knuth)
             .GetGameFlow()
             .GetDealFlow(new DealNumber(2))
             
+            .GetCards(WhichPlayer.Knuth, new DealNumber(3))
+            .GetCards(WhichPlayer.Graham, new DealNumber(3))
             .BidPair(WhichPlayer.Knuth, FaceCard.King)
             .BidPair(WhichPlayer.Graham, FaceCard.Ace)
             .Check(WhichPlayer.Knuth)
             .GetGameFlow()
             .GetDealFlow(new DealNumber(3))
             
+            .GetCards(WhichPlayer.Knuth, new DealNumber(4))
+            .GetCards(WhichPlayer.Graham, new DealNumber(4))
             .BidTwoPairs(WhichPlayer.Graham, FaceCard.Nine, FaceCard.Ten)
             .BidFourOfAKind(WhichPlayer.Knuth, FaceCard.Nine)
             .Check(WhichPlayer.Graham)
             .GetGameFlow()
             .GetDealFlow(new DealNumber(4))
+            
+            .GetCards(WhichPlayer.Knuth, new DealNumber(5))
+            .GetCards(WhichPlayer.Graham, new DealNumber(5))
+            .BidHighCard(WhichPlayer.Knuth, FaceCard.Queen)
+            .BidHighCard(WhichPlayer.Graham, FaceCard.King)
+            .BidPair(WhichPlayer.Knuth, FaceCard.Queen)
+            .BidPair(WhichPlayer.Graham, FaceCard.King)
+            .BidFullHouse(WhichPlayer.Knuth, FaceCard.Ace, FaceCard.King)
+            .Check(WhichPlayer.Graham)
+            .GetGameFlow()
+            .GetDealFlow(new DealNumber(5))
             
             .Build();
         
