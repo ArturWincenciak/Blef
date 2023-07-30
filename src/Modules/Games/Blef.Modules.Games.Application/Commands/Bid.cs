e1deabc6ffb2d49c7267219a7f06dc2f177087ca
TeoVincent <artur.wincenciak@gmail.com>
2023-07-30 23:13:04 +0200

    todo: use everywhere the domain objects instead of primitives

--- src/Modules/Games/Blef.Modules.Games.Application/Commands/Bid.cs
+++ src/Modules/Games/Blef.Modules.Games.Application/Commands/Bid.cs
@@ -2,6 +2,8 @@
 
 namespace Blef.Modules.Games.Application.Commands;
 
+// todo: use everywhere the domain objects instead of primitives
+// here in only one example place, but it should be everywhere
 public sealed record Bid(Guid GameId, Guid PlayerId, string PokerHand) : ICommand<Bid.Result>
 {
     public sealed record Result(int DealNumber) : ICommandResult;
