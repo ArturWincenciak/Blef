8499120bec8766d5a4aa677f0515b95b12ae5233
TeoVincent <artur.wincenciak@gmail.com>
2023-07-30 23:16:22 +0200

    todo: define exception for domain layer without any problem details structure

--- src/Shared/Blef.Shared.Kernel/Exceptions/BlefException.cs
+++ src/Shared/Blef.Shared.Kernel/Exceptions/BlefException.cs
@@ -3,6 +3,10 @@ using System.Text.Json;
 
 namespace Blef.Shared.Kernel.Exceptions;
 
+// todo: define exception for domain layer without any problem details structure
+// domain should not know about problem details
+// because domain layer don't know about API layer
+// domain don't know about the HTTP on API layer
 public abstract class BlefException : Exception
 {
     public string Title { get; }
