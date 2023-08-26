4738a2a4ab2367c9e23705498d7e4499b535a5cd
TeoVincent <artur.wincenciak@gmail.com>
2023-08-26 10:53:33 +0200

    Cleanup: refactor

---
+++ tests/Blef.Modules.Games.Api.Tests/Scenarios/HappyPath/FourPlayersPlayTheGame.cs
@@ -0,0 +1,28 @@
+﻿using Blef.Modules.Games.Api.Tests.Core;
+using Blef.Modules.Games.Api.Tests.Scenarios.ValueObjects;
+
+namespace Blef.Modules.Games.Api.Tests.Scenarios.HappyPath;
+
+[UsesVerify]
+public class FourPlayersPlayTheGame
+{
+    [Fact]
+    public async Task Scenario()
+    {
+        var results = await new TestBuilder()
+
+            .NewGame()
+            .GetGameFlow()
+            .JoinPlayer(WhichPlayer.Knuth)
+            .JoinPlayer(WhichPlayer.Graham)
+            .JoinPlayer(WhichPlayer.Conway)
+            .JoinPlayer(WhichPlayer.Riemann)
+            .NewDeal()
+            .GetGameFlow()
+            .GetDealFlow(new DealNumber(1))
+
+            .Build();
+
+        await Verify(results);
+    }
+}
