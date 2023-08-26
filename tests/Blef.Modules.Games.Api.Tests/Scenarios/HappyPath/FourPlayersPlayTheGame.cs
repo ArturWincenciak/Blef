0e9d6aff11e38bc6bbc23c9675820a64edc987ef
TeoVincent <artur.wincenciak@gmail.com>
2023-08-26 12:35:13 +0200

    Tests: four players play the game

--- tests/Blef.Modules.Games.Api.Tests/Scenarios/HappyPath/FourPlayersPlayTheGame.cs
+++ tests/Blef.Modules.Games.Api.Tests/Scenarios/HappyPath/FourPlayersPlayTheGame.cs
@@ -21,6 +21,51 @@ public class FourPlayersPlayTheGame
             .GetGameFlow()
             .GetDealFlow(new DealNumber(1))
 
+            .GetCards(WhichPlayer.Knuth, new DealNumber(1))
+            .GetCards(WhichPlayer.Graham, new DealNumber(1))
+            .GetCards(WhichPlayer.Conway, new DealNumber(1))
+            .GetCards(WhichPlayer.Riemann, new DealNumber(1))
+            .BidHighCard(WhichPlayer.Knuth, FaceCard.Nine)
+            .BidHighCard(WhichPlayer.Graham, FaceCard.Ten)
+            .BidHighCard(WhichPlayer.Conway, FaceCard.Jack)
+            .BidHighCard(WhichPlayer.Riemann, FaceCard.Queen)
+            .Check(WhichPlayer.Knuth)
+            .GetGameFlow()
+            .GetDealFlow(deal: new DealNumber(1))
+
+            .GetCards(WhichPlayer.Knuth, new DealNumber(2))
+            .GetCards(WhichPlayer.Graham, new DealNumber(2))
+            .GetCards(WhichPlayer.Conway, new DealNumber(2))
+            .GetCards(WhichPlayer.Riemann, new DealNumber(2))
+            .BidPair(WhichPlayer.Graham, FaceCard.Ace)
+            .Check(WhichPlayer.Conway)
+            .GetGameFlow()
+            .GetDealFlow(deal: new DealNumber(2))
+
+            .GetCards(WhichPlayer.Knuth, new DealNumber(3))
+            .GetCards(WhichPlayer.Graham, new DealNumber(3))
+            .GetCards(WhichPlayer.Conway, new DealNumber(3))
+            .GetCards(WhichPlayer.Riemann, new DealNumber(3))
+            .BidPair(WhichPlayer.Conway, FaceCard.King)
+            .BidThreeOfAKind(WhichPlayer.Knuth, FaceCard.Ace)
+            .BidThreeOfAKind(WhichPlayer.Riemann, FaceCard.Ace)
+            .Check(WhichPlayer.Knuth)
+            .GetGameFlow()
+            .GetDealFlow(deal: new DealNumber(3))
+
+            .GetCards(WhichPlayer.Knuth, new DealNumber(4))
+            .GetCards(WhichPlayer.Graham, new DealNumber(4))
+            .GetCards(WhichPlayer.Conway, new DealNumber(4))
+            .GetCards(WhichPlayer.Riemann, new DealNumber(4))
+            .BidFourOfAKind(WhichPlayer.Knuth, FaceCard.Nine)
+            .BidStraightFlush(WhichPlayer.Riemann, Suit.Clubs)
+            .Check(WhichPlayer.Conway)
+            .BidHighStraight(WhichPlayer.Knuth)
+            .Check(WhichPlayer.Graham) // todo: here is a bug, here should be rejected move
+            // .Check(WhichPlayer.Knuth) // todo: here is a bug, here is correct move
+            // .GetGameFlow()
+            // .GetDealFlow(deal: new DealNumber(4))
+
             .Build();
 
         await Verify(results);
