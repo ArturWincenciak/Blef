b567dcace170ef78ed88c30e98a776cbd1beb069
TeoVincent <artur.wincenciak@gmail.com>
2023-09-03 22:49:34 +0200

    Test: Cannot check out of order policy and when the deal has not been started

--- src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/NoBidToCheckException.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Exceptions/NoBidToCheckException.cs
@@ -8,7 +8,7 @@ internal sealed class NoBidToCheckException : BlefException
     public NoBidToCheckException(DealId dealId)
         : base(
             title: "There is no bid to check it",
-            detail: "Game must be started by at least one bid, cannot check when the game has not started")
+            detail: $"Deal number '{dealId.Deal.Number}' must be started by at least one bid")
     {
     }
 }
