46b4b3524451b6890f6ea7ac678eb43f466e694f
TeoVincent <artur.wincenciak@gmail.com>
2023-10-14 21:29:04 +0200

    Tests: tests of enumerator cases

---
+++ tests/Blef.Modules.Games.Domain.Tests/IsOnTableTests/Cases/TestsOfEnumeratorCases.cs
@@ -0,0 +1,18 @@
+﻿namespace Blef.Modules.Games.Domain.Tests.IsOnTableTests.Cases;
+
+public class TestsOfEnumeratorCases
+{
+    [Fact]
+    public void GetPokerHandIsOnTheTableCasesEnumeratorTest()
+    {
+        using var enumerator = new PokerHandIsOnTheTableCases().GetEnumerator();
+        Assert.NotNull(enumerator);
+    }
+
+    [Fact]
+    public void GetPokerHandIsNotOnTheTableCasesEnumeratorTest()
+    {
+        using var enumerator = new PokerHandIsNotOnTheTableCases().GetEnumerator();
+        Assert.NotNull(enumerator);
+    }
+}
