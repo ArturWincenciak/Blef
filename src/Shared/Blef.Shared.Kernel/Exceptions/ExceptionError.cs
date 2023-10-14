a2624845e7621ef4f624b002601805c04c30deb1
TeoVincent <artur.wincenciak@gmail.com>
2023-10-14 23:32:07 +0200

    Cleanup: remove unused code

--- src/Shared/Blef.Shared.Kernel/Exceptions/ExceptionError.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace Blef.Shared.Kernel.Exceptions;
-
-[SuppressMessage(category: "ReSharper", checkId: "ClassNeverInstantiated.Global")]
-public sealed record ExceptionError(string Code, string[] Values);
