5c7fd6722e922d9490a79cb2b210204a188ce49e
TeoVincent <artur.wincenciak@gmail.com>
2023-10-14 10:47:27 +0200

    Tests: map poker hand test when unknown type of poker hand should throw argument out of range exception

---
+++ tests/Blef.Modules.Games.Domain.Tests/IsOnTableTests/Tests/MapPokerHandTest.cs
@@ -0,0 +1,19 @@
+﻿using Blef.Modules.Games.Domain.Model.PokerHands;
+
+namespace Blef.Modules.Games.Domain.Tests.IsOnTableTests.Tests;
+
+public class MapPokerHandTest
+{
+    [Fact]
+    internal void MapPokerHandTest_WhenUnknownTypeOfPokerHand_ShouldThrowArgumentOutOfRangeException()
+    {
+        // arrange
+        const string bid = "unknown:ace";
+
+        // act
+        PokerHand Act() => PokerHand.Map(bid);
+
+        // assert
+        Assert.Throws<ArgumentOutOfRangeException>((Func<PokerHand>?) Act ?? throw new Exception());
+    }
+}
