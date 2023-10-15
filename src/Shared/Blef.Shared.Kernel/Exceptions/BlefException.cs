caa867084baf7140e1e50848fd8b56f5ad4097c6
TeoVincent <artur.wincenciak@gmail.com>
2023-10-15 23:28:09 +0200

    Cleanup: remove not needed code

--- src/Shared/Blef.Shared.Kernel/Exceptions/BlefException.cs
+++ src/Shared/Blef.Shared.Kernel/Exceptions/BlefException.cs
@@ -12,14 +12,12 @@ public abstract class BlefException : Exception
     public string Title { get; }
     public string Detail { get; }
     public string Instance { get; private set; }
-    public IDictionary<string, string[]> Errors { get; }
 
     protected BlefException(string title, string detail)
         : base(JsonSerializer.Serialize(new {title, detail}))
     {
         Title = title;
         Detail = detail;
-        Errors = new Dictionary<string, string[]>();
         Instance = string.Empty;
     }
 
