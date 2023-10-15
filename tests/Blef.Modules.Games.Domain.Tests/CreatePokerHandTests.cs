927688501da5e10a5187f9df941a50356ce3b426
TeoVincent <artur.wincenciak@gmail.com>
2023-10-15 08:53:28 +0200

    Tests: create two full house hand with the same face cards should throw exception

---
+++ tests/Blef.Modules.Games.Domain.Tests/CreatePokerHandTests.cs
@@ -0,0 +1,31 @@
+﻿using Blef.Modules.Games.Domain.Model;
+using Blef.Modules.Games.Domain.Model.PokerHands;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class CreatePokerHandTests
+{
+    [Fact]
+    public void Given_FaceCardsAreTheSame_When_CreateTwoPairs_Then_ShouldThrowException()
+    {
+        // arrange
+        var firstFaceCard = FaceCard.Ace;
+        var secondFaceCard = FaceCard.Ace;
+
+        // act, assert
+        Assert.Throws<ArgumentException>(() =>
+            TwoPairs.Create($"{firstFaceCard},{secondFaceCard}"));
+    }
+
+    [Fact]
+    public void Given_FaceCardsAreTheSame_When_CreateFullHouse_Then_ShouldThrowException()
+    {
+        // arrange
+        var threeOfAKind = FaceCard.Ace;
+        var pair = FaceCard.Ace;
+
+        // act, assert
+        Assert.Throws<ArgumentException>(() =>
+            FullHouse.Create($"{threeOfAKind},{pair}"));
+    }
+}
