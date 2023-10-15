f1fae1efa9df9b683af272dc017c604df4207aeb
TeoVincent <artur.wincenciak@gmail.com>
2023-10-16 00:35:43 +0200

    Tests: internal server error

---
+++ tests/Blef.Bootstrapper.Tests/Mocks/ThrowGameplayRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blef.Modules.Games.Application.Repositories;
+using Blef.Modules.Games.Domain.Model;
+
+namespace Blef.Bootstrapper.Tests.Mocks;
+
+internal sealed class ThrowGameplayRepository : IGameplaysRepository
+{
+    public Task Add(Gameplay gameplay) =>
+        Task.CompletedTask;
+
+    public Task<Gameplay?> Get(GameId gameId) =>
+        throw new Exception("Test exception on get gameplay");
+}
