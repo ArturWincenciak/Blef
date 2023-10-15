b245b3cfeadeccaf7a602ef97df62a7ade8e9de9
TeoVincent <artur.wincenciak@gmail.com>
2023-10-15 08:50:21 +0200

    Tests: create two pairs poker hand with the same face cards should throw enxception

---
+++ tests/Blef.Modules.Games.Domain.Tests/CreateTwoPairsPokerHandTests.cs
@@ -0,0 +1,19 @@
+﻿using Blef.Modules.Games.Domain.Model;
+using Blef.Modules.Games.Domain.Model.PokerHands;
+
+namespace Blef.Modules.Games.Domain.Tests;
+
+public class CreateTwoPairsPokerHandTests
+{
+    [Fact]
+    public void ShouldThrowExceptionWhenFaceCardsAreTheSame()
+    {
+        // arrange
+        var firstFaceCard = FaceCard.Ace;
+        var secondFaceCard = FaceCard.Ace;
+
+        // act, assert
+        Assert.Throws<ArgumentException>(() =>
+            TwoPairs.Create($"{firstFaceCard},{secondFaceCard}"));
+    }
+}
