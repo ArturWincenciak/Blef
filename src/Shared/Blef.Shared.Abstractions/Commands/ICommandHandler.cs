b2cc6c69b42cf512b956042ec1b2045b263472bc
TeoVincent <artur.wincenciak@gmail.com>
2023-10-19 18:48:13 +0200

    Cleanup: remove unused ICommandHandler abstraction without returning result

--- src/Shared/Blef.Shared.Abstractions/Commands/ICommandHandler.cs
+++ src/Shared/Blef.Shared.Abstractions/Commands/ICommandHandler.cs
@@ -2,11 +2,7 @@
 
 namespace Blef.Shared.Abstractions.Commands;
 
-public interface ICommandHandler<in TCommand>
-    where TCommand : ICommand
-{
-    Task Handle(TCommand command, CancellationToken cancellation);
-}
+
 
 public interface ICommandHandler<in TCommand, TCommandResult>
     where TCommand : ICommand<TCommandResult>
