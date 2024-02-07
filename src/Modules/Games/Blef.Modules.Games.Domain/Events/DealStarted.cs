2983d9920eb801e91e2cf100d03cefb675b7bf8b
TeoVincent Artur Wincenciak <artur.wincenciak@gmail.com>
2024-02-08 00:53:27 +0100

    Cleanup: re-format code by JetBrains CleanupCode Tool

--- src/Modules/Games/Blef.Modules.Games.Domain/Events/DealStarted.cs
+++ src/Modules/Games/Blef.Modules.Games.Domain/Events/DealStarted.cs
@@ -4,7 +4,7 @@ using Blef.Shared.Abstractions.Events;
 namespace Blef.Modules.Games.Domain.Events;
 
 internal sealed record DealStarted(
-        GameId Game,
-        DealNumber Deal,
-        IReadOnlyCollection<DealPlayer> Players)
+    GameId Game,
+    DealNumber Deal,
+    IReadOnlyCollection<DealPlayer> Players)
     : IDomainEvent<DealStarted>;
